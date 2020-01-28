a4033c1d49fccf73823a0bafce14a365d53f8893
Kishan Rathore <Kishan.Rathore2@mindtree.com>
2020-01-28 17:50:35 +0530

    MOS-31019_Language conde validation at controller layer

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/WorkingDayController.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/WorkingDayController.java
@@ -1,7 +1,5 @@
 package io.mosip.kernel.masterdata.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,9 +45,8 @@ public class WorkingDayController {
 			@ApiResponse(code = 200, message = "When Week Days retrieved from database for the given Registration center ID Languge Code"),
 			@ApiResponse(code = 404, message = "When No Week Days found for the given Registration center ID and Languge Code"),
 			@ApiResponse(code = 500, message = "While retrieving Week Days any error occured") })
-	public ResponseWrapper<WeekDaysResponseDto> getWeekDays(
-			@PathVariable("registrationCenterId")  String regCenterId,@ValidLangCode(message = "Language Code is Invalid")
-			@PathVariable("langCode")  String langCode) {
+	public ResponseWrapper<WeekDaysResponseDto> getWeekDays(@PathVariable("registrationCenterId") String regCenterId,
+			@ValidLangCode(message = "Language Code is Invalid") @PathVariable("langCode") String langCode) {
 
 		ResponseWrapper<WeekDaysResponseDto> responseWrapper = new ResponseWrapper<>();
 		responseWrapper.setResponse(service.getWeekDaysList(regCenterId, langCode));
@@ -73,7 +70,8 @@ public class WorkingDayController {
 			@ApiResponse(code = 404, message = "When No working days found for the given Registration center ID and lang Code"),
 			@ApiResponse(code = 500, message = "While retrieving working days any error occured") })
 	public ResponseWrapper<WorkingDaysResponseDto> getWorkingays(
-			@PathVariable("registrationCenterID") String regCenterId, @PathVariable("langCode") String langCode) {
+			@PathVariable("registrationCenterID") String regCenterId,
+			@ValidLangCode(message = "Language Code is Invalid") @PathVariable("langCode") String langCode) {
 
 		ResponseWrapper<WorkingDaysResponseDto> responseWrapper = new ResponseWrapper<>();
 		responseWrapper.setResponse(service.getWorkingDays(regCenterId, langCode));
