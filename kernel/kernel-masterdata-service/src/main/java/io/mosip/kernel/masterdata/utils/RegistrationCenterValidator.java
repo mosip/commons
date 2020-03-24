bb1ed23bbe64ed6a1525483048bce4523d5e5c12
Jyoti Prakash Nayak <M1030448@mindtree.com>
2020-03-24 10:37:41 +0530

    MOS-31006_code refactoring

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/utils/RegistrationCenterValidator.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/utils/RegistrationCenterValidator.java
@@ -858,8 +858,7 @@ public class RegistrationCenterValidator {
 			List<String> zoneIds) {
 		String centerZoneCode=registrationCenterRepository.findByIdAndLangCode(registrationCenterDto.getId(), registrationCenterDto.getLangCode()).getZoneCode();
 		if (!zoneIds.isEmpty()) {
-			//if (!zoneIds.contains(registrationCenterDto.getZoneCode())) {
-			if (!zoneIds.contains(centerZoneCode)) { 
+			if (!zoneIds.contains(centerZoneCode) || !zoneIds.contains(registrationCenterDto.getZoneCode())) { 
 				errors.add(new ServiceError(RegistrationCenterErrorCode.INVALIDE_ZONE.getErrorCode(),
 						String.format(RegistrationCenterErrorCode.INVALIDE_ZONE.getErrorMessage(),
 								registrationCenterDto.getZoneCode())));
