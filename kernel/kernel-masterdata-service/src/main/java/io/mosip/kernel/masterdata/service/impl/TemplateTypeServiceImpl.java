405b6f0567963e34a2fdfcb8ced146ff6b74e4f2
MeghaUttamTanga <megha.tanga@mindtree.com>
2020-04-16 15:39:54 +0530

    MOS-710 coding to get the list of template type from DB based on given lang-code and template type code

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/TemplateTypeServiceImpl.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/TemplateTypeServiceImpl.java
@@ -1,5 +1,7 @@
 package io.mosip.kernel.masterdata.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -7,8 +9,10 @@ import org.springframework.stereotype.Service;
 import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
 import io.mosip.kernel.masterdata.constant.TemplateTypeErrorCode;
 import io.mosip.kernel.masterdata.dto.TemplateTypeDto;
+import io.mosip.kernel.masterdata.dto.TemplateTypeResponseDto;
 import io.mosip.kernel.masterdata.entity.TemplateType;
 import io.mosip.kernel.masterdata.entity.id.CodeAndLanguageCodeID;
+import io.mosip.kernel.masterdata.exception.DataNotFoundException;
 import io.mosip.kernel.masterdata.exception.MasterDataServiceException;
 import io.mosip.kernel.masterdata.repository.TemplateTypeRepository;
 import io.mosip.kernel.masterdata.service.TemplateTypeService;
@@ -19,6 +23,7 @@ import io.mosip.kernel.masterdata.utils.MetaDataUtils;
 /**
  * 
  * @author Uday Kumar
+ * @author Megha Tanga
  * @since 1.0.0
  *
  */
@@ -53,6 +58,66 @@ public class TemplateTypeServiceImpl implements TemplateTypeService {
 		MapperUtils.map(templateType, codeLangCodeId);
 
 		return codeLangCodeId;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.mosip.kernel.masterdata.service.TemplateTypeService#
+	 * getTemplateTypeCodeandLangCode(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public TemplateTypeResponseDto getTemplateTypeCodeandLangCode(String templateTypeCode, String langCode) {
+		List<TemplateType> templateTypeList = null;
+		List<TemplateTypeDto> templateTypeDtoList = null;
+		TemplateTypeResponseDto templateTypeResponseIdDto = new TemplateTypeResponseDto();
+		try {
+			templateTypeList = templateTypeRepository
+					.findAllByCodeAndLangCodeAndIsDeletedFalseorIsDeletedIsNull(templateTypeCode, langCode);
+		} catch (DataAccessException | DataAccessLayerException e) {
+			throw new MasterDataServiceException(TemplateTypeErrorCode.TEMPLATE_TYPE_FETCH_EXCEPTION.getErrorCode(),
+					TemplateTypeErrorCode.TEMPLATE_TYPE_FETCH_EXCEPTION.getErrorMessage()
+							+ ExceptionUtils.parseException(e));
+		}
+		if (templateTypeList != null && !templateTypeList.isEmpty()) {
+			templateTypeDtoList = MapperUtils.mapAll(templateTypeList, TemplateTypeDto.class);
+		} else {
+
+			throw new DataNotFoundException(TemplateTypeErrorCode.TEMPLATE_TYPE_NOT_FOUND_EXCEPTION.getErrorCode(),
+					TemplateTypeErrorCode.TEMPLATE_TYPE_NOT_FOUND_EXCEPTION.getErrorMessage());
+
+		}
+		templateTypeResponseIdDto.setTemplateTypes(templateTypeDtoList);
+		return templateTypeResponseIdDto;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.mosip.kernel.masterdata.service.TemplateTypeService#
+	 * getTemplateTypeLangCode(java.lang.String)
+	 */
+	@Override
+	public TemplateTypeResponseDto getTemplateTypeLangCode(String langCode) {
+		TemplateTypeResponseDto templateTypeResponseDto = new TemplateTypeResponseDto();
+		List<TemplateType> templateTypeList = null;
+		List<TemplateTypeDto> templateTypeDtoList = null;
+		try {
+			templateTypeList = templateTypeRepository.findAllByLangCodeAndIsDeletedFalseOrIsDeletedIsNull(langCode);
+		} catch (DataAccessException | DataAccessLayerException e) {
+			throw new MasterDataServiceException(TemplateTypeErrorCode.TEMPLATE_TYPE_FETCH_EXCEPTION.getErrorCode(),
+					TemplateTypeErrorCode.TEMPLATE_TYPE_FETCH_EXCEPTION.getErrorMessage()
+							+ ExceptionUtils.parseException(e));
+		}
+		if (templateTypeList != null && !templateTypeList.isEmpty()) {
+			templateTypeDtoList = MapperUtils.mapAll(templateTypeList, TemplateTypeDto.class);
+
+		} else {
+			throw new DataNotFoundException(TemplateTypeErrorCode.TEMPLATE_TYPE_NOT_FOUND_EXCEPTION.getErrorCode(),
+					TemplateTypeErrorCode.TEMPLATE_TYPE_NOT_FOUND_EXCEPTION.getErrorMessage());
+		}
+		templateTypeResponseDto.setTemplateTypes(templateTypeDtoList);
+		return templateTypeResponseDto;
 	}
 
 }
