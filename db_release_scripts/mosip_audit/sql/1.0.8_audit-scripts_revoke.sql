73241566e0be6309d2170629489be08f9326b494
sadanandegowda <43214830+sadanandegowda@users.noreply.github.com>
2020-04-27 19:04:26 +0530

    [MOSIP-723]-[MOSIP-817] - DB Changes for 1.0.9 release (#167)

---
+++ db_release_scripts/mosip_audit/sql/1.0.8_audit-scripts_revoke.sql
@@ -0,0 +1,11 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_audit
+-- Release Version 	: 1.0.8
+-- Purpose    		: Revoking Database Alter deployement done for release in Audit DB.       
+-- Create By   		: Sadanandegowda DM
+-- Created Date		: 16-Apr-2020
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -------------------------------------------------------------------------------------------------
+
+\c mosip_audit sysadmin
---
+++ db_release_scripts/mosip_master/sql/1.0.9_master-scripts_release.sql
@@ -0,0 +1,33 @@
+-- ---------------------------------------------------------------------------------------------------------
+-- Database Name: mosip_master
+-- Release Version 	: 1.0.9
+-- Purpose    		: Database Alter scripts for the release for Master DB.       
+-- Create By   		: Sadanandegowda DM
+-- Created Date		: 23-Apr-2020
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -----------------------------------------------------------------------------------------------------------
+
+-- ------------------------------------------------------------------------------------------------------------
+\c mosip_master sysadmin
+
+\ir ../ddl/master-loc_hierarchy_list.sql
+
+\ir ../ddl/master-schema_definition.sql
+\ir ../ddl/master-dynamic_field.sql
+\ir ../ddl/master-identity_schema.sql
+
+
+----- TRUNCATE master.template_type TABLE Data and It's reference Data and COPY Data from CSV file -----
+TRUNCATE TABLE master.loc_hierarchy_list cascade ;
+
+\COPY master.loc_hierarchy_list (hierarchy_level,hierarchy_level_name,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-loc_hierarchy_list.csv' delimiter ',' HEADER  csv;
+
+
+-- object: fk_loc_lochierlst | type: CONSTRAINT --
+-- ALTER TABLE master.location DROP CONSTRAINT IF EXISTS fk_loc_lochierlst CASCADE;
+ALTER TABLE master.location ADD CONSTRAINT fk_loc_lochierlst FOREIGN KEY (hierarchy_level,hierarchy_level_name,lang_code)
+REFERENCES master.loc_hierarchy_list (hierarchy_level,hierarchy_level_name,lang_code) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+----------------------------------------------------------------------------------------------------
