ea88b52cb2c764fb9e09cbf52104111eeaa14432
johnpsmindtree <43216416+johnpsmindtree@users.noreply.github.com>
2020-07-01 20:57:14 +0530

    1.0.9 release rc1 (#312)
    * Removed "-rc1" from all pom.xml files
    
    * removed -rc1 in id-repository
    
    Co-authored-by: M1045585 <M1045585@A2ML26116.mindtree.com>
    Co-authored-by: John David Panneerselvam <M1045585@mindtree.com>

---
+++ kernel/kernel-jasperreport-service/src/main/java/io/mosip/kernel/jasperreport/dto/JasperreportResponseDto.java
@@ -0,0 +1,18 @@
+package io.mosip.kernel.jasperreport.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JasperreportResponseDto {
+
+	/**
+	 * The boolean audit status
+	 */
+	private boolean status;
+
+}
