eddf02df430a5ab135b246eac75f212db393b5f9
Nagarjuna K <nagarjunabtechece@gmail.com>
2020-07-28 16:10:46 +0530

    Update ConnectionUtilTest.java

--- kernel/kernel-fsadapter-hdfs/src/test/java/io/mosip/kernal/fsadapter/hdfs/test/util/ConnectionUtilTest.java
+++ kernel/kernel-fsadapter-hdfs/src/test/java/io/mosip/kernal/fsadapter/hdfs/test/util/ConnectionUtilTest.java
@@ -8,6 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
