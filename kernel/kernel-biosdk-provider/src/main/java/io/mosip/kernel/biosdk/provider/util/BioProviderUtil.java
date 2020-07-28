ec9336c1a056766c35351ed28e55308065bd5072
Manoj SP <Manoj.SP@mindtree.com>
2020-07-28 13:19:08 +0530

    [MOSIP-8129] updated logic to check instance key

--- kernel/kernel-biosdk-provider/src/main/java/io/mosip/kernel/biosdk/provider/util/BioProviderUtil.java
+++ kernel/kernel-biosdk-provider/src/main/java/io/mosip/kernel/biosdk/provider/util/BioProviderUtil.java
@@ -20,7 +20,8 @@ public class BioProviderUtil {
 
 	public static Object getSDKInstance(Map<String, String> modalityParams) throws BiometricException {
 		try {
-			String instanceKey = modalityParams.values().stream().collect(Collectors.joining());
+			String instanceKey = modalityParams.entrySet().stream().sorted(Map.Entry.comparingByKey())
+					.map(entry -> entry.getKey() + entry.getValue()).collect(Collectors.joining());
 			if (sdkInstances.containsKey(instanceKey)) {
 				LOGGER.debug(ProviderConstants.LOGGER_SESSIONID, ProviderConstants.LOGGER_IDTYPE, "GET SDK INSTANCE",
 						"SDK instance reused for modality class >>> "
