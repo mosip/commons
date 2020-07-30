1ab487c8e341a9b9efdd77bb177c4ca29fb09b33
urviljoshi <urvvil08@gmail.com>
2020-07-30 13:15:20 +0530

    rebasing with develop

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/authmanager/model/UserPasswordRequestDto.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/authmanager/model/UserPasswordRequestDto.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserPasswordRequestDto {
-	
 	@NotBlank(message = AuthConstant.INVALID_REQUEST)
 	private String appId;
 	@NotBlank(message = AuthConstant.INVALID_REQUEST)
