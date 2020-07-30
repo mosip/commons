b3595386b70e71d4c17a9232ae906baa17695cb0
urviljoshi <urvvil08@gmail.com>
2020-07-30 13:14:56 +0530

    rebasing with develop

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/authmanager/model/MosipUserDto.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/authmanager/model/MosipUserDto.java
@@ -5,13 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * MOSIP USER IS THE STANDARD SPEC THAT WILL BE TUNED BASED ON THE DETAILS
- * STORED IN LDAP FOR A USER
- *
  * @author Sabbu Uday Kumar
  * @since 1.0.0
  */
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
