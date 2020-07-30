7d1dd9fbd30d3aaadca890ee4d9402c3db0a9ea3
urviljoshi <urvvil08@gmail.com>
2020-07-30 14:13:04 +0530

    rebasing with develop

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/adapter/handler/AuthHandler.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/adapter/handler/AuthHandler.java
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
