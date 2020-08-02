ed97e8667a8be3356f3470135a5b2d63b4316769
urviljoshi <urvvil08@gmail.com>
2020-08-03 05:14:08 +0530

    change to external package mode

---
+++ kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/defaultimpl/dto/UserDetailsRequestDto.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package io.mosip.kernel.auth.defaultimpl.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * @author Ramadurai Pandian
+ *
+ */
+@Data
+public class UserDetailsRequestDto {
+
+	List<String> userDetails;
+
+}
