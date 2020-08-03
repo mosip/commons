a57e182b5f0ad256424ca5818511af8616b4af72
urviljoshi <urvvil08@gmail.com>
2020-08-03 05:52:16 +0530

    changes in auth adapter

--- kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/AuthBootApplication.java
+++ kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/AuthBootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"io.mosip.kernel.auth.controller","io.mosip.kernel.auth.config","${mosip.iam.impl.package}"})
+@SpringBootApplication(scanBasePackages = {"io.mosip.kernel.auth.controller","io.mosip.kernel.auth.config","io.mosip.kernel.auth.adapter.*","${mosip.iam.impl.package}"})
 public class AuthBootApplication {
 
 	public static void main(String[] args) {
