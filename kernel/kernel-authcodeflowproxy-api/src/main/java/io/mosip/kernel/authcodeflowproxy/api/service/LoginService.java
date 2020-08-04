5c10c7e2c4cb6a9e941020c9c4ddc44cb441b925
urviljoshi <urvvil08@gmail.com>
2020-08-05 01:00:09 +0530

    added validate token to proxy auth code api

--- kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/service/LoginService.java
+++ kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/service/LoginService.java
@@ -2,10 +2,14 @@ package io.mosip.kernel.authcodeflowproxy.api.service;
 
 import javax.servlet.http.Cookie;
 
+import io.mosip.kernel.authcodeflowproxy.api.dto.MosipUserDto;
+
 public interface LoginService {
 
 	String login(String redirectURI, String state);
 
 	Cookie createCookie(String authCookie);
+
+	MosipUserDto valdiateToken(String authToken);
 
 }
