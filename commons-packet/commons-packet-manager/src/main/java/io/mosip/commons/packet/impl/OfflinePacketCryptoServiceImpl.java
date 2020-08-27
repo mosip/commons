7f91a6f81a5eb988a5b9ba7b4f348dc092a3f510
Monobikash Das <M1045447@mindtree.com>
2020-08-27 16:17:23 +0530

    MOSIP-8064 : offline crypto

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/impl/OfflinePacketCryptoServiceImpl.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/impl/OfflinePacketCryptoServiceImpl.java
@@ -1,50 +1,37 @@
 package io.mosip.commons.packet.impl;
 
 import io.mosip.commons.packet.spi.IPacketCryptoService;
-import io.mosip.kernel.core.crypto.spi.CryptoCoreSpec;
-import io.mosip.kernel.keygenerator.bouncycastle.KeyGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 @Component
 @Qualifier("OfflinePacketCryptoServiceImpl")
 public class OfflinePacketCryptoServiceImpl implements IPacketCryptoService {
 
-    @Value("${mosip.kernel.data-key-splitter:KEY_SPLITTER}")
-    private String KEY_SPLITTER;
-
-    @Autowired
-    private KeyGenerator keyGenerator;
-
-    @Autowired
-    private CryptoCoreSpec<byte[], byte[], SecretKey, PublicKey, PrivateKey, String> cryptoCore;
+    /*@Autowired
+    ClientCryptoManagerService clientCryptoManagerService;*/
 
     @Override
     public byte[] sign(byte[] packet) {
-        // TODO : To be implemented
+        //return clientCryptoManagerService.csSign(packet);
         return new byte[0];
     }
 
     @Override
     public byte[] encrypt(String id, byte[] packet) {
+        //return clientCryptoManagerService.csEncrypt(packet);
         return packet;
     }
 
     @Override
     public byte[] decrypt(String id, byte[] packet) {
-        // TODO : To be implemented
+        //return clientCryptoManagerService.csDecrypt(packet);
         return packet;
     }
 
     @Override
-    public boolean verify(byte[] packet) {
-        // TODO : To be implemented
+    public boolean verify(byte[] packet, byte[] signature) {
+        //return clientCryptoManagerService.csVerify(packet, signature);
         return true;
     }
 }
