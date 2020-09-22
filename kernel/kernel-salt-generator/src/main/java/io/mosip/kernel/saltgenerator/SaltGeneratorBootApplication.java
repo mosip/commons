d6f0ec13664b0df471701fb02798257b9efd33ba
Manoj SP <43261486+manojsp12@users.noreply.github.com>
2020-09-22 13:21:25 +0530

    updated salt generator (#472)
    * [MOSIP-8129] Removed Redundant instantiation of SDK with same class
    
    * [MOSIP-8129] updated rest helper
    
    * [MOSIP-8129] updated rest helper
    
    * [MOSIP-8129] added log
    
    * [MOSIP-8129] updated version
    
    * [MOSIP-8129] updated var name
    
    * [MOSIP-8129] updated bioproviderutil
    
    * [MOSIP-8129] updated logic to check instance key
    
    * [MOSIP-8129] updated bioprovider util
    
    * [MOSIP-8129] updated bioprovider util
    
    * [MOSIP-979] updated salt generator
    
    * Revert "[MOSIP-979] updated salt generator"
    
    This reverts commit 64effa6c5665474b37c98c979d39d7e41b5825ec.
    
    * [MOSIP-979] updated salt generator
    
    Co-authored-by: Urvil Joshi <42868205+urviljoshi@users.noreply.github.com>

--- kernel/kernel-salt-generator/src/main/java/io/mosip/kernel/saltgenerator/SaltGeneratorBootApplication.java
+++ kernel/kernel-salt-generator/src/main/java/io/mosip/kernel/saltgenerator/SaltGeneratorBootApplication.java
@@ -2,17 +2,21 @@ package io.mosip.kernel.saltgenerator;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.scheduling.ScheduledTasksEndpointAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
 /**
- * The Class SaltGeneratorBootApplication - Salt generator Job is a
- * one-time job which populates salts for hashing and encrypting data.
+ * The Class SaltGeneratorBootApplication - Salt generator Job is a one-time job
+ * which populates salts for hashing and encrypting data.
  *
  * @author Manoj SP
  */
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableAutoConfiguration(exclude = { JmxAutoConfiguration.class, ScheduledTasksEndpointAutoConfiguration.class })
 public class SaltGeneratorBootApplication {
 
 	/**
@@ -21,8 +25,7 @@ public class SaltGeneratorBootApplication {
 	 * @param args the arguments
 	 */
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SaltGeneratorBootApplication.class,
-				args);
+		ApplicationContext applicationContext = SpringApplication.run(SaltGeneratorBootApplication.class, args);
 		SpringApplication.exit(applicationContext);
 	}
 
