02976835f8829be2b57878b2cbf30b54b56af7de
ase-101 <>
2020-10-05 11:17:18 +0530

    changes to support integration of keymanager-service without any datasource configuration, Config.java is renamed to keymanagerConfig.java as it was conflicting with pre-reg integration

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/config/KeymanagerDaoConfig.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/config/KeymanagerDaoConfig.java
@@ -14,6 +14,7 @@ import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -31,7 +32,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import io.mosip.kernel.keymanagerservice.constant.HibernatePersistenceConstant;
 
 
-
+@ConditionalOnProperty(value = "mosip.keymanager.dao.enabled", matchIfMissing = true)
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
