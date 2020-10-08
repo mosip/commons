6f7741b7d169463c2c5e16affa1d69e88d12aaea
Monobikash Das <M1045447@mindtree.com>
2020-10-08 16:01:04 +0530

    MOSIP-8064 : disabling signature temporarily

--- kernel/object-store/src/main/java/io/mosip/commons/khazana/util/OfflineEncryptionUtil.java
+++ kernel/object-store/src/main/java/io/mosip/commons/khazana/util/OfflineEncryptionUtil.java
@@ -79,7 +79,7 @@ public class OfflineEncryptionUtil {
         System.arraycopy(nonce, 0, finalEncData, 0, nonce.length);
         System.arraycopy(aad, 0, finalEncData, nonce.length, aad.length);
         System.arraycopy(encryptedData, 0, finalEncData, nonce.length + aad.length,	encryptedData.length);
-        return encryptedData;
+        return finalEncData;
     }
 
     private CryptomanagerService getCryptomanagerService() {
