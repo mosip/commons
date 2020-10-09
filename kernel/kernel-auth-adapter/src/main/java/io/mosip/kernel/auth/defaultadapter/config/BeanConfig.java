1b81eaa75dc2dabd3f7f282a68860c67497fdf35
Tabish Khan <tabish.khansfc@gmail.com>
2020-10-09 10:59:21 +0530

    removed unused imports

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/BeanConfig.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/BeanConfig.java
@@ -22,9 +22,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import ch.qos.logback.classic.helpers.MDCInsertingServletFilter;
 import io.mosip.kernel.auth.defaultadapter.constant.AuthAdapterConstant;
-import io.mosip.kernel.auth.defaultadapter.filter.TraceLogFilter;
 import io.mosip.kernel.core.authmanager.authadapter.model.AuthUserDetails;
 
 
