c17afec1354e96b869b98f70b93d0933b21dac23
Sada <sadanandegowda.dm@mindtree.com>
2020-10-09 19:55:54 +0530

    Updated alter scripts for machine master table change

--- db_release_scripts/mosip_master/sql/1.1.2_master-scripts_release.sql
+++ db_release_scripts/mosip_master/sql/1.1.2_master-scripts_release.sql
@@ -48,10 +48,15 @@ ALTER TABLE master.machine_master_h ADD COLUMN IF NOT EXISTS regcntr_id characte
 ALTER TABLE master.user_detail_h ADD COLUMN IF NOT EXISTS regcntr_id character varying(10);
 
 ALTER TABLE master.machine_master ADD COLUMN IF NOT EXISTS sign_public_key character varying(1024);
-ALTER TABLE master.machine_master_h ADD COLUMN IF NOT EXISTS sign_key_index character varying(128);
+ALTER TABLE master.machine_master ADD COLUMN IF NOT EXISTS sign_key_index character varying(128);
+ALTER TABLE master.machine_master ALTER COLUMN public_key DROP NOT NULL;
+ALTER TABLE master.machine_master ALTER COLUMN key_index DROP NOT NULL;
 
-ALTER TABLE master.machine_master ALTER COLUMN public_key TYPE character varying(1024);
-ALTER TABLE master.machine_master_h ALTER COLUMN key_index TYPE character varying(128);
+ALTER TABLE master.machine_master_h ADD COLUMN IF NOT EXISTS sign_public_key character varying(1024);
+ALTER TABLE master.machine_master_h ADD COLUMN IF NOT EXISTS sign_key_index character varying(128);
+ALTER TABLE master.machine_master_h ALTER COLUMN public_key DROP NOT NULL;
+ALTER TABLE master.machine_master_h ALTER COLUMN key_index DROP NOT NULL;
+
 
 -- object: fk_machm_center | type: CONSTRAINT --
 ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_center CASCADE;
