7d2280a1c389bca10282738d4f658d059ee8c9c4
Monobikash Das <M1045447@mindtree.com>
2020-10-18 14:56:52 +0530

    MOSIP-9781 : fixing minio connection issue

--- kernel/object-store/src/main/java/io/mosip/commons/khazana/constant/KhazanaErrorCodes.java
+++ kernel/object-store/src/main/java/io/mosip/commons/khazana/constant/KhazanaErrorCodes.java
@@ -3,7 +3,8 @@ package io.mosip.commons.khazana.constant;
 public enum KhazanaErrorCodes {
 
     CONTAINER_NOT_PRESENT_IN_DESTINATION("COM-KZN-001", "Container not found."),
-    ENCRYPTION_FAILURE("COM-KZN-002", "Packet Encryption Failed-Invalid Packet format");
+    ENCRYPTION_FAILURE("COM-KZN-002", "Packet Encryption Failed-Invalid Packet format"),
+    OBJECT_STORE_NOT_ACCESSIBLE("COM-KZN-003", "Object store not accessible");
 
 
     private final String errorCode;
