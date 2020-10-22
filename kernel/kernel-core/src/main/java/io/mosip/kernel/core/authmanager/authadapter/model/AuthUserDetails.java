58565260fb6b5cf3220ea1ed0af1dad13d6eeea7
Tabish Khan <tabish.khansfc@gmail.com>
2020-10-22 13:22:42 +0530

    Fixed kernel import

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/authmanager/authadapter/model/AuthUserDetails.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/authmanager/authadapter/model/AuthUserDetails.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import io.mosip.kernel.core.authmanager.model.MosipUserDto;
+
 
 
 
