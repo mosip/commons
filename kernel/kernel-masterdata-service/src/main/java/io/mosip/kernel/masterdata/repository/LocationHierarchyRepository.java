7c9276b39d506501661bdc326321d70226fdb048
jpnayak007 <43367563+jpnayak007@users.noreply.github.com>
2020-10-30 14:37:49 +0530

    Location_blocker_fix (#555)
    Co-authored-by: Jyoti Prakash Nayak <M1030448@mindtree.com>

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/LocationHierarchyRepository.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/LocationHierarchyRepository.java
@@ -48,6 +48,6 @@ public interface LocationHierarchyRepository extends BaseRepository<LocationHier
 	@Query("FROM LocationHierarchy l where l.langCode = ?1 and hierarchyLevel = ?2 and hierarchyLevelName =?3 and (l.isDeleted is null or l.isDeleted = false) and l.isActive = true")
 	LocationHierarchy findByLangCodeAndLevelAndName(String langCode,short hierarchyLevel,String hierarchyLevelName);
 
-	@Query(value = "select hierarchy_level FROM loc_hierarchy_list where hierarchy_level_name = :heirarchyLevelName and lang_code=:languageCode and (is_deleted is null or is_deleted = false) and is_active = true", nativeQuery = true)
+	@Query(value = "select hierarchy_level FROM master.loc_hierarchy_list where hierarchy_level_name = :heirarchyLevelName and lang_code=:languageCode and (is_deleted is null or is_deleted = false) and is_active = true", nativeQuery = true)
 	Integer findByheirarchyLevalNameAndLangCode(@Param("heirarchyLevelName") String heirarchyLevelName,@Param("languageCode") String languageCode);
 }
