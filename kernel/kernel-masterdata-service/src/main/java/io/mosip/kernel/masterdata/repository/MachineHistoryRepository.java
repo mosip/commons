e7120be08962d6827d12852f57090e64ed8b1ef7
Jyoti Prakash Nayak <M1030448@mindtree.com>
2020-11-17 18:54:50 +0530

    MOSIP-10479_modified the sql queries

--- kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/MachineHistoryRepository.java
+++ kernel/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/MachineHistoryRepository.java
@@ -34,7 +34,7 @@ public interface MachineHistoryRepository extends BaseRepository<MachineHistory,
 	List<MachineHistory> findByFirstByIdAndLangCodeAndEffectDtimesLessThanEqualAndIsDeletedFalseOrIsDeletedIsNull(
 			String id, String langCode, LocalDateTime effectDtimes);
 
-	@Query(value = "Select 	m.eff_dtimes, m.id,m.cr_by, m.cr_dtimes, m.del_dtimes, m.is_active, m.is_deleted, m.upd_by, m.upd_dtimes, m.ip_address, m.lang_code, m.mac_address, m.mspec_id, m.name,  m.serial_num, m.validity_end_dtimes, m.zone_code,m.regcntr_id, m.key_index, m.public_key, m.sign_public_key, m.sign_key_index from master.machine_master_h m where m.regcntr_id = ?1 and m.id = ?2 and m.eff_dtimes <= ?3 and ( m.is_deleted = false or m.is_deleted is null) order by m.eff_dtimes desc limit 1", nativeQuery = true)
+	@Query(value = "Select 	m.eff_dtimes, m.id,m.cr_by, m.cr_dtimes, m.del_dtimes, m.is_active, m.is_deleted, m.upd_by, m.upd_dtimes, m.ip_address, m.lang_code, m.mac_address, m.mspec_id, m.name,  m.serial_num, m.validity_end_dtimes, m.zone_code,m.regcntr_id, m.key_index, m.public_key, m.sign_public_key, m.sign_key_index from master.machine_master_h m where m.regcntr_id = ?1 and m.id = ?2 and m.eff_dtimes <= ?3 and ( m.is_deleted = false or m.is_deleted is null) order by m.eff_dtimes desc ", nativeQuery = true)
 	List<MachineHistory> findByCntrIdAndMachineIdAndEffectivetimesLessThanEqualAndIsDeletedFalseOrIsDeletedIsNull(
 			String registrationCenterId, String machineId, LocalDateTime lDateAndTime);
 
