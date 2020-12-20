621eb66bfb322df661feb6931d6d158da29bb80f
Shilpa M <m1053288@mindtree.com>
2020-12-20 19:44:32 +0530

    MOSIP-10946,Include Applicant Type as the first field to collect the applicant type details

--- kernel/kernel-applicanttype-api/src/main/java/io/mosip/kernel/applicanttype/api/impl/ApplicantTypeImpl.java
+++ kernel/kernel-applicanttype-api/src/main/java/io/mosip/kernel/applicanttype/api/impl/ApplicantTypeImpl.java
@@ -1,16 +1,19 @@
 package io.mosip.kernel.applicanttype.api.impl;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.util.Map;
-import java.util.Map.Entry;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mvel2.MVEL;
+import org.mvel2.integration.VariableResolverFactory;
+import org.mvel2.integration.impl.MapVariableResolverFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import io.mosip.kernel.applicanttype.api.constant.ApplicantTypeErrorCode;
 import io.mosip.kernel.core.applicanttype.exception.InvalidApplicantArgumentException;
@@ -26,20 +29,18 @@ import io.mosip.kernel.core.applicanttype.spi.ApplicantType;
 public class ApplicantTypeImpl implements ApplicantType {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicantTypeImpl.class);
-	private static final String UTC_DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
-	private static final String FOREIGNER = "FR";
-	private static final String NON_FOREIGNER = "NFR";
-	private static final String MALE = "MLE";
-	private static final String FEMALE = "FLE";
-	private static final String CHILD = "CHL";
-	private static final String ADULT = "ADL";
-	private static final String ATTR_INDIVIDUAL_TYPE = "individualTypeCode";
-	private static final String ATTR_DATE_OF_BIRTH = "dateofbirth";
-	private static final String ATTR_GENDER_TYPE = "genderCode";
-	private static final String ATTR_BIOMETRIC_EXCEPTION_TYPE = "biometricAvailable";
 	@Value("${mosip.kernel.applicant.type.age.limit}")
 	private String ageLimit;
+
+	@Value("${mosip.kernel.config.server.file.storage.uri}")
+	private String configServerFileStorageURL;
+
+	@Value("${mosip.kernel.applicantType.mvel.file}")
+	private String mvelFile;
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	/*
 	 * (non-Javadoc)
@@ -48,167 +49,40 @@ public class ApplicantTypeImpl implements ApplicantType {
 	 * io.mosip.kernel.core.applicanttype.spi.ApplicantCodeService#getApplicantType(
 	 * java.util.Map)
 	 */
-	public String getApplicantType(Map<String, Object> map) throws InvalidApplicantArgumentException {
-		// queries to be check
-		String itc = null;// Individual Type Code
-		String dob = null;
-		String genderType = null;
-		boolean isBioExPresent = false;// Biometric Exception Type
-
-		// insert the values for queries
-		for (Entry<String, Object> entry : map.entrySet()) {
-			if (entry.getKey().equals(ATTR_INDIVIDUAL_TYPE) && entry.getValue() instanceof String) {
-				itc = (String) entry.getValue();
-			} else if (entry.getKey().equals(ATTR_DATE_OF_BIRTH) && entry.getValue() instanceof String) {
-				dob = (String) entry.getValue();
-			} else if (entry.getKey().equals(ATTR_GENDER_TYPE) && entry.getValue() instanceof String) {
-				genderType = (String) entry.getValue();
-			} else if (entry.getKey().equals(ATTR_BIOMETRIC_EXCEPTION_TYPE) && entry.getValue() != null) {
-				// for defect id MOS-17562
-				if (entry.getValue() instanceof String) {
-					String str = (String) entry.getValue();
-					if (str.trim().equalsIgnoreCase("true")) {
-						isBioExPresent = true;
-					} else if (str.trim().equalsIgnoreCase("false")) {
-						isBioExPresent = false;
-					} else {
-						throw new InvalidApplicantArgumentException(
-								ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorCode(),
-								ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorMessage());
-					}
-				} else if (entry.getValue() instanceof Boolean) {
-					isBioExPresent = (boolean) entry.getValue();
-				} else {
-					throw new InvalidApplicantArgumentException(
-							ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorCode(),
-							ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorMessage());
-				}
-			}
-		}
-
-		// check for NPE
-		if (isNullEmpty(itc) || isNullEmpty(genderType) || isNullEmpty(dob)) {
-			LOGGER.error("Illegal argument passed to applicant type");
-			throw new InvalidApplicantArgumentException(ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorCode(),
-					ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorMessage());
-
-		}
-
-		// calculate age
-		Integer age = null;
+	public String getApplicantType(Map<String, Object> m) throws InvalidApplicantArgumentException {
+		LOGGER.info("Getting code for applicant type");
 		try {
-			age = calculateAge(dob);
-			if (age == null || age < 0) {
+			LOGGER.info("Getting data from MVEL file->"+configServerFileStorageURL + mvelFile);
+			String mvelExpression = restTemplate.getForObject(configServerFileStorageURL + mvelFile, String.class);
+
+			Map<String, Object> context = new HashMap();
+
+			context.put("map", m);
+			context.put("agelimit", ageLimit == null ? "18" : ageLimit);
+
+			VariableResolverFactory functionFactory = new MapVariableResolverFactory();
+			MVEL.eval(mvelExpression, context, functionFactory);
+
+			VariableResolverFactory myVarFactory = new MapVariableResolverFactory();
+			myVarFactory.setNextFactory(functionFactory);
+
+			Serializable s = MVEL.compileExpression("getApplicantType(map,agelimit);", context);
+
+			String code = (String) MVEL.executeExpression(s, m, myVarFactory);
+
+			if (code.equalsIgnoreCase("KER-MSD-147")) {
+				LOGGER.error("Error while fetching applicant code");
 				throw new InvalidApplicantArgumentException(
 						ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorCode(),
 						ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorMessage());
 			}
-		} catch (Exception e) {
-			LOGGER.error("Error while claculating age");
-			throw new InvalidApplicantArgumentException(
-					ApplicantTypeErrorCode.INVALID_DATE_STRING_EXCEPTION.getErrorCode(),
-					ApplicantTypeErrorCode.INVALID_DATE_STRING_EXCEPTION.getErrorMessage(), e);
+			LOGGER.info("Code for applicant type  is " + code);
+			return code;
+		} catch (Exception ex) {
+			LOGGER.error("Error while executing mvel file" + ex.getMessage());
+			throw ex;
 		}
-
-		String ageCode = null;
-
-		try {
-			if (age >= Integer.parseInt(ageLimit)) {
-				ageCode = ADULT;
-			}
-			if (age >= 0 && age < Integer.parseInt(ageLimit)) {
-				ageCode = CHILD;
-			}
-		} catch (NumberFormatException e) {
-			LOGGER.error("Error while setting age code");
-			throw new InvalidApplicantArgumentException(ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorCode(),
-					ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorMessage(), e);
-		}
-
-		// check and return the applicant id
-		return findApplicantType(itc, genderType, isBioExPresent, ageCode);
-	}
-
-	private boolean isNullEmpty(String str) {
-		return str == null || str.trim().length() <= 0;
-	}
-
-	private String findApplicantType(String itc, String genderType, boolean isBioExPresent, String ageCode) {
-		if (itc.equals(FOREIGNER) && genderType.equals(MALE) && ageCode.equals(CHILD) && !isBioExPresent) {
-			// 1
-			return "001";
-		} else if (itc.equals(FOREIGNER) && genderType.equals(MALE) && ageCode.equals(ADULT) && !isBioExPresent) {
-			// 2
-			return "002";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(MALE) && ageCode.equals(CHILD) && !isBioExPresent) {
-			// 3
-			return "003";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(MALE) && ageCode.equals(ADULT) && !isBioExPresent) {
-			// 4
-			return "004";
-
-		} else if (itc.equals(FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(CHILD) && !isBioExPresent) {
-			// 5
-			return "005";
-
-		} else if (itc.equals(FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(ADULT) && !isBioExPresent) {
-			// 6
-			return "006";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(CHILD) && !isBioExPresent) {
-			// 7
-			return "007";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(ADULT) && !isBioExPresent) {
-			// 8
-			return "008";
-
-		} else if (itc.equals(FOREIGNER) && genderType.equals(MALE) && ageCode.equals(CHILD) && isBioExPresent) {
-			// 9
-			return "009";
-
-		} else if (itc.equals(FOREIGNER) && genderType.equals(MALE) && ageCode.equals(ADULT) && isBioExPresent) {
-			// 10
-			return "010";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(MALE) && ageCode.equals(CHILD) && isBioExPresent) {
-			// 11
-			return "011";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(MALE) && ageCode.equals(ADULT) && isBioExPresent) {
-			// 12
-			return "012";
-
-		} else if (itc.equals(FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(CHILD) && isBioExPresent) {
-			// 13
-			return "013";
-
-		} else if (itc.equals(FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(ADULT) && isBioExPresent) {
-			// 14
-			return "014";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(CHILD) && isBioExPresent) {
-			// 15
-			return "015";
-
-		} else if (itc.equals(NON_FOREIGNER) && genderType.equals(FEMALE) && ageCode.equals(ADULT) && isBioExPresent) {
-			// 16
-			return "016";
-
-		}
-		return null;
-	}
-
-	private int calculateAge(String dob) {
-		int age = -1;
-		LocalDate birthDate = LocalDateTime.parse(dob, DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN)).toLocalDate();
-		LocalDate currentDate = LocalDate.now();
-		if (birthDate != null && currentDate != null && !birthDate.isAfter(currentDate)) {
-			age = Period.between(birthDate, currentDate).getYears();
-		}
-		return age;
+		
 	}
 
 }
