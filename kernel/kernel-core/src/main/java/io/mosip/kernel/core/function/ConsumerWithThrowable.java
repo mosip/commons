64fb0cbff06904facf89baa1750543b7ce1f4625
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-02-09 15:16:16 +0530

    Added retry support and utility

---
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/function/ConsumerWithThrowable.java
@@ -0,0 +1,23 @@
+package io.mosip.kernel.core.function;
+
+/**
+ * Consumer Functional interface which can throw Exception.
+ *
+ * @param <T> the generic type
+ * @param <E> the element type that can be any Throwable
+ * 
+ * @author Loganathan Sekar
+ * 
+ */
+@FunctionalInterface
+public interface ConsumerWithThrowable<T, E extends Throwable> {
+
+	/**
+	 * Expression that accepts the given arguments.
+	 *
+	 * @param t the first function argument
+	 * @throws E the exception
+	 */
+	void accept(T t) throws E;
+
+}
