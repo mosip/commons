f6e013f08131cd501770424e710f4b81c0341656
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2021-03-02 17:51:03 +0530

    MOSIP-12259 : fixed schema validation issue (#717)
    * MOSIP-11634 : making BiometricRecord object serializable to cache using hazelcast
    
    * MOSIP-12259 : fixed schema validation issue
    
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/impl/PacketReaderImpl.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/impl/PacketReaderImpl.java
@@ -98,7 +98,7 @@ public class PacketReaderImpl implements IPacketReader {
 	@Override
 	public boolean validatePacket(String id, String source, String process) {
 		try {
-			return packetValidator.validate(id, source, process, getAll(id, source, process));
+			return packetValidator.validate(id, source, process);
 		} catch (BaseCheckedException | IOException | NoSuchAlgorithmException e) {
 			LOGGER.error(PacketManagerLogger.SESSIONID, PacketManagerLogger.REGISTRATIONID, id,
 					"Packet Validation exception : " + ExceptionUtils.getStackTrace(e));
