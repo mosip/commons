72003678fe44a5acfe96f319e8272a1c3c773038
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-03-22 12:34:43 +0530

    @rambhatt1591

--- db_release_scripts/mosip_iam/sql/1.1.5_iam-scripts_release.sql
+++ db_release_scripts/mosip_iam/sql/1.1.5_iam-scripts_release.sql
@@ -7,24 +7,26 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 --
--- -------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+-- Mar-2021		Ram Bhatt	   Reverting is_deleted not null changes
+----------------------------------------------------------------------------------------------------
 
 \c mosip_iam sysadmin
 
 -- -------------------------------------------------------------------------------------------------
 
-ALTER TABLE iam.oauth_access_token ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE iam.role_list ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE iam.user_detail ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE iam.user_detail_h ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE iam.user_pwd ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE iam.user_role ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE iam.oauth_access_token ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE iam.role_list ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE iam.user_detail ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE iam.user_detail_h ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE iam.user_pwd ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE iam.user_role ALTER COLUMN is_deleted SET NOT NULL;
 
-ALTER TABLE iam.oauth_access_token ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE iam.role_list ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE iam.user_detail ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE iam.user_detail_h ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE iam.user_pwd ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE iam.user_role ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE iam.oauth_access_token ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE iam.role_list ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE iam.user_detail ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE iam.user_detail_h ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE iam.user_pwd ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE iam.user_role ALTER COLUMN is_deleted SET DEFAULT FALSE;
 
 ----------------------------------------------------------------------------------------------------
--- db_release_scripts/mosip_kernel/sql/1.1.5_kernel-scripts_release.sql
+++ db_release_scripts/mosip_kernel/sql/1.1.5_kernel-scripts_release.sql
@@ -7,20 +7,22 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
+-- Mar-2021		Ram Bhatt	    Reverting is_deleted not null changes
+----------------------------------------------------------------------------------------------------
 
 \c mosip_kernel sysadmin
 
-ALTER TABLE kernel.otp_transaction ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE kernel.prid ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE kernel.uin_assigned ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE kernel.vid ALTER COLUMN is_deleted SET NOT NULL;
-ALTER TABLE kernel.uin ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE kernel.otp_transaction ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE kernel.prid ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE kernel.uin_assigned ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE kernel.vid ALTER COLUMN is_deleted SET NOT NULL;
+--ALTER TABLE kernel.uin ALTER COLUMN is_deleted SET NOT NULL;
 
-ALTER TABLE kernel.otp_transaction ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE kernel.prid ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE kernel.uin_assigned ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE kernel.vid ALTER COLUMN is_deleted SET DEFAULT FALSE;
-ALTER TABLE kernel.uin ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE kernel.otp_transaction ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE kernel.prid ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE kernel.uin_assigned ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE kernel.vid ALTER COLUMN is_deleted SET DEFAULT FALSE;
+--ALTER TABLE kernel.uin ALTER COLUMN is_deleted SET DEFAULT FALSE;
 
 
 
