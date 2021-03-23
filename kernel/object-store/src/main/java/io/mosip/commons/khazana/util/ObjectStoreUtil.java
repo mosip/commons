ca32cbd3f0a8c19fcc2396bd52cf8f3c06ae9adc
M1022006 <M1022006@A2ML18485.mindtree.com>
2021-03-23 17:27:25 +0530

    MOSIP-12368 1.1.4.1 code changes

--- kernel/object-store/src/main/java/io/mosip/commons/khazana/util/ObjectStoreUtil.java
+++ kernel/object-store/src/main/java/io/mosip/commons/khazana/util/ObjectStoreUtil.java
@@ -17,4 +17,17 @@ public class ObjectStoreUtil {
 
         return finalObjectName;
     }
+    public static String getName(String container,String source, String process, String objectName) {
+        String finalObjectName = "";
+        if (StringUtils.isNotEmpty(container))
+            finalObjectName = container + SEPARATOR;
+        if (StringUtils.isNotEmpty(source))
+            finalObjectName = finalObjectName + source + SEPARATOR;
+        if (StringUtils.isNotEmpty(process))
+            finalObjectName = finalObjectName + process + SEPARATOR;
+
+        finalObjectName = finalObjectName + objectName;
+
+        return finalObjectName;
+    }
 }
