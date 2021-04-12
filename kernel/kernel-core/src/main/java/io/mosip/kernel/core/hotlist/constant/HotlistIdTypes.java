4f01f50ea0d8f623afb71ec2593b4cbbb0d1767c
Manoj SP <Manoj.SP@mindtree.com>
2021-04-12 19:28:35 +0530

    [MOSIP-11791] added hotlist constants

---
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/hotlist/constant/HotlistIdTypes.java
@@ -0,0 +1,26 @@
+package io.mosip.kernel.core.hotlist.constant;
+
+/**
+ * @author Manoj SP
+ *
+ */
+public class HotlistIdTypes {
+
+	public static String UIN = "UIN";
+	
+	public static String VID = "VID";
+	
+	public static String MACHINE_ID = "MACHINE_ID";
+	
+	public static String PARTNER_ID = "PARTNER_ID";
+	
+	public static String OPERATOR_ID = "OPERATOR_ID";
+	
+	public static String CENTER_ID = "CENTER_ID";
+	
+	public static String DEVICE = "DEVICE";
+	
+	public static String DEVICE_PROVIDER = "DEVICE_PROVIDER";
+	
+	public static String FTM_PUBLIC_KEY = "FTM_PUBLIC_KEY";
+}
---
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/hotlist/constant/HotlistStatus.java
@@ -0,0 +1,15 @@
+package io.mosip.kernel.core.hotlist.constant;
+
+/**
+ * The Class HotlistStatus.
+ *
+ * @author Manoj SP
+ */
+public class HotlistStatus {
+
+	/** The blocked. */
+	public static String BLOCKED = "BLOCKED";
+
+	/** The unblocked. */
+	public static String UNBLOCKED = "UNBLOCKED";
+}
