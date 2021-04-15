f8d810feff71c7f46a5664f0b1a6569300ba943c
Manoj SP <43261486+manojsp12@users.noreply.github.com>
2021-04-15 10:17:38 +0530

    [MOSIP-11791] added hotlist constants (#781)
    * [MOSIP-11791] added hotlist constants
    
    * [MOSIP-11791] updated constants

---
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/hotlist/constant/HotlistIdTypes.java
@@ -0,0 +1,26 @@
+package io.mosip.kernel.core.hotlist.constant;
+
+/**
+ * @author Manoj SP
+ *
+ */
+public class HotlistIdTypes {
+
+	public static final String UIN = "UIN";
+	
+	public static final String VID = "VID";
+	
+	public static final String MACHINE_ID = "MACHINE_ID";
+	
+	public static final String PARTNER_ID = "PARTNER_ID";
+	
+	public static final String OPERATOR_ID = "OPERATOR_ID";
+	
+	public static final String CENTER_ID = "CENTER_ID";
+	
+	public static final String DEVICE = "DEVICE";
+	
+	public static final String DEVICE_PROVIDER = "DEVICE_PROVIDER";
+	
+	public static final String FTM_PUBLIC_KEY = "FTM_PUBLIC_KEY";
+}
---
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/hotlist/constant/HotlistStatus.java
@@ -0,0 +1,15 @@
+package io.mosip.kernel.core.hotlist.constant;
+
+/**
+ * The Class HotlistStatus.
+ *
+ * @author Manoj SP
+ */
+public class HotlistStatus {
+
+	/** The blocked. */
+	public static final String BLOCKED = "BLOCKED";
+
+	/** The unblocked. */
+	public static final String UNBLOCKED = "UNBLOCKED";
+}
