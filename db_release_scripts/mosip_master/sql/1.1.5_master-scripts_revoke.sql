ac0bfbf68ca3b2e0906a311b22d34b7d0cb44a2a
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-04-26 14:59:33 +0530

    MOSIP-13552 Updated dynamic field and identity schema csv for 1.1.5 release

--- db_release_scripts/mosip_master/sql/1.1.5_master-scripts_revoke.sql
+++ db_release_scripts/mosip_master/sql/1.1.5_master-scripts_revoke.sql
@@ -3,7 +3,7 @@
 -- Release Version 	: 1.1.5
 -- Purpose    		: Revoking Database Alter deployement done for release in Master DB.       
 -- Create By   		: Ram Bhatt
--- Created Date		: Jan-2021
+-- Created Date		: Apr-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -----------------------------------------------------------------------------------------------------------
@@ -11,5 +11,11 @@
 -- -----------------------------------------------------------------------------------------------------------
 \c mosip_master sysadmin
 
+TRUNCATE TABLE master.dynamic_field cascade ;
 
+\COPY master.dynamic_field (id,name,description,data_type,value_json,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-dynamic_field.csv' delimiter ',' HEADER  csv;
+
+TRUNCATE TABLE master.identity_schema cascade ;
+
+\COPY master.identity_schema (id,id_version,title,description,id_attr_json,schema_json,status_code,add_props,effective_from,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-identity_schema.csv' delimiter ',' HEADER  csv;
 --------------------------------------------------------------------------------------------------------------
