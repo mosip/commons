fde6cd711b9f653622ede122191f9c2850691633
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-04-26 15:00:03 +0530

    MOSIP-13552 Updated dynamic field and identity schema csv for 1.1.5 release

--- db_release_scripts/mosip_master/sql/1.1.5_master-scripts_revoke.sql
+++ db_release_scripts/mosip_master/sql/1.1.5_master-scripts_revoke.sql
@@ -13,9 +13,9 @@
 
 TRUNCATE TABLE master.dynamic_field cascade ;
 
-\COPY master.dynamic_field (id,name,description,data_type,value_json,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-dynamic_field.csv' delimiter ',' HEADER  csv;
+\COPY master.dynamic_field (id,name,description,data_type,value_json,lang_code,is_active,cr_by,cr_dtimes) FROM '../dml/master-dynamic_field.csv' delimiter ',' HEADER  csv;
 
 TRUNCATE TABLE master.identity_schema cascade ;
 
-\COPY master.identity_schema (id,id_version,title,description,id_attr_json,schema_json,status_code,add_props,effective_from,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-identity_schema.csv' delimiter ',' HEADER  csv;
+\COPY master.identity_schema (id,id_version,title,description,id_attr_json,schema_json,status_code,add_props,effective_from,lang_code,is_active,cr_by,cr_dtimes) FROM '../dml/master-identity_schema.csv' delimiter ',' HEADER  csv;
 --------------------------------------------------------------------------------------------------------------
