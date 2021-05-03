1fc54b2c664253940b61bdb290ab700de087dd2d
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-05-03 09:09:35 +0530

    MOSIP-13444 Revert is deleted not null changes for authdevice

--- db_scripts/mosip_authdevice/ddl/authdevice-registered_device_master_h.sql
+++ db_scripts/mosip_authdevice/ddl/authdevice-registered_device_master_h.sql
@@ -33,7 +33,7 @@ CREATE TABLE authdevice.registered_device_master_h(
 	cr_dtimes timestamp NOT NULL,
 	upd_by character varying(256),
 	upd_dtimes timestamp,
-	is_deleted boolean NOT NULL DEFAULT FALSE,
+	is_deleted boolean DEFAULT FALSE,
 	del_dtimes timestamp,
 	eff_dtimes timestamp NOT NULL,
 	CONSTRAINT pk_regdevicemh_code PRIMARY KEY (code,eff_dtimes)
