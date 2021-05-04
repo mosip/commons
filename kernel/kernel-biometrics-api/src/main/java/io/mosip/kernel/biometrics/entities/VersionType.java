094959a0a9b02bdf1f2e81f11287b56bb3f6f104
M1022006 <M1022006@A2ML18485.mindtree.com>
2021-05-04 11:14:00 +0530

    Biometric api changes fixed

--- kernel/kernel-biometrics-api/src/main/java/io/mosip/kernel/biometrics/entities/VersionType.java
+++ kernel/kernel-biometrics-api/src/main/java/io/mosip/kernel/biometrics/entities/VersionType.java
@@ -19,7 +19,10 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 @Data
+@JsonDeserialize(builder = VersionType.VersionTypeBuilder.class)
 @AllArgsConstructor
 @NoArgsConstructor
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -32,6 +35,28 @@ public class VersionType implements Serializable {
 	@XmlSchemaType(name = "unsignedInt")
 	private int minor;
 	
+	public VersionType(VersionTypeBuilder versionTypeBuilder) {
+		this.major = versionTypeBuilder.major;
+		this.minor = versionTypeBuilder.minor;
+	}
+	public static class VersionTypeBuilder {
+		private int major;
+		private int minor;
+
+		public VersionTypeBuilder withMinor(int minor) {
+			this.minor = minor;
+			return this;
+		}
+
+		public VersionTypeBuilder withMajor(int major) {
+			this.major = major;
+			return this;
+		}
+
+		public VersionType build() {
+			return new VersionType(this);
+		}
+	}
 
 
 }
