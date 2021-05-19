e5ff7297d5605b2f10bbc75195086b730c9a6774
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-05-19 13:02:27 +0530

    MOSIP-13258 Additional History Table Changes (#840)

--- db_scripts/mosip_master/ddl/master-user_detail_h.sql
+++ db_scripts/mosip_master/ddl/master-user_detail_h.sql
@@ -10,6 +10,7 @@
 -- ------------------------------------------------------------------------------------------
 -- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false
 -- Mar-2021		Ram Bhatt	    Reverting is_deleted not null changes
+-- May-2021		Ram Bhatt	    Change LANG code TO NULLABLE AND remove from Primary Key
 -- ------------------------------------------------------------------------------------------
 
 -- object: master.user_detail_h | type: TABLE --
@@ -22,7 +23,7 @@ CREATE TABLE master.user_detail_h(
 	mobile character varying(16),
 	status_code character varying(36) NOT NULL,
 	regcntr_id character varying(10),
-	lang_code character varying(3) NOT NULL,
+	lang_code character varying(3),
 	last_login_dtimes timestamp,
 	last_login_method character varying(64),
 	is_active boolean NOT NULL,
