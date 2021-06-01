3af6522bc9e1e004faa1a29d9a5d5848b15ff83d
urviljoshi <urvvil08@gmail.com>
2021-06-01 16:31:24 +0530

    MOSIP-14269 added option to skip reused token

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/RequesterTokenRestInterceptor.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/RequesterTokenRestInterceptor.java
@@ -2,6 +2,7 @@ package io.mosip.kernel.auth.defaultadapter.config;
 
 import java.io.IOException;
 
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -14,21 +15,32 @@ import io.mosip.kernel.core.authmanager.authadapter.model.AuthUserDetails;
 
 public class RequesterTokenRestInterceptor implements ClientHttpRequestInterceptor {
 
+	/*
+	 * This config is introduced as a patch to fix the use of expired token from
+	 * security context rather than taking token from actual request. Later proper
+	 * default behaviour to be coded and this config to be removed
+	 */
+	//TODO
+	private boolean skipContextTokenReuse;
+
+	public RequesterTokenRestInterceptor(Environment environment) {
+		skipContextTokenReuse = environment.getProperty("auth.adapter.rest.template.skip.context.token.reuse",
+				Boolean.class, false);
+	}
+
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
 			throws IOException {
 		addHeadersToRequest(request, body);
 		return execution.execute(request, body);
 	}
-	
 
 	private void addHeadersToRequest(HttpRequest httpRequest, byte[] bytes) {
 		HttpHeaders headers = httpRequest.getHeaders();
-			AuthUserDetails authUserDetails = getAuthUserDetails();
-			if (authUserDetails != null)
+		AuthUserDetails authUserDetails = getAuthUserDetails();
+			if (!skipContextTokenReuse && authUserDetails != null)
 				headers.set(AuthAdapterConstant.AUTH_HEADER_COOKIE,
-				AuthAdapterConstant.AUTH_HEADER_COOKIE + authUserDetails.getToken());
-		
+						AuthAdapterConstant.AUTH_HEADER_COOKIE + authUserDetails.getToken());
 	}
 
 	private AuthUserDetails getAuthUserDetails() {
@@ -39,7 +51,7 @@ public class RequesterTokenRestInterceptor implements ClientHttpRequestIntercept
 			authUserDetails = (AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		return authUserDetails;
 	}
-	
+
 	// This methods set token send by resource server to context
 	private void getHeadersFromResponse(ClientHttpResponse clientHttpResponse) {
 		HttpHeaders headers = clientHttpResponse.getHeaders();
