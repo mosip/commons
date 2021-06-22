8793068d71b467fb94a993eed255fae5cd00a87b
urviljoshi <urvvil08@gmail.com>
2021-06-22 19:10:13 +0530

    MOSIP-15125 pagination also added to signature

--- kernel/kernel-websubclient-api/src/main/java/io/mosip/kernel/websub/api/client/SubscriberClientImpl.java
+++ kernel/kernel-websubclient-api/src/main/java/io/mosip/kernel/websub/api/client/SubscriberClientImpl.java
@@ -167,16 +167,17 @@ public class SubscriberClientImpl
 
 	@Override
 	public FailedContentResponse getFailedContent(FailedContentRequest failedContentRequest) {
+		int pageIndex = failedContentRequest.getPaginationIndex() < 0 ? 0 : failedContentRequest.getPaginationIndex();
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 		if (failedContentRequest.getMessageCount() > 0 && failedContentRequest.getPaginationIndex()>0) {
 			headers.set(WebSubClientConstants.SUBSCRIBER_SIGNATURE_HEADER,
-					getHmac256(failedContentRequest.getTopic() + failedContentRequest.getCallbackURL()+ failedContentRequest.getTimestamp()+ String.valueOf(failedContentRequest.getPaginationIndex())+String.valueOf(failedContentRequest.getMessageCount()),
+					getHmac256(failedContentRequest.getTopic() + failedContentRequest.getCallbackURL()+ failedContentRequest.getTimestamp()+String.valueOf(pageIndex)+ String.valueOf(failedContentRequest.getPaginationIndex())+String.valueOf(failedContentRequest.getMessageCount()),
 							failedContentRequest.getSecret()));
 		} else {
 			headers.set(WebSubClientConstants.SUBSCRIBER_SIGNATURE_HEADER,
 					getHmac256(failedContentRequest.getTopic() + failedContentRequest.getCallbackURL()
-							+ failedContentRequest.getTimestamp(), failedContentRequest.getSecret()));
+							+ failedContentRequest.getTimestamp()+String.valueOf(pageIndex), failedContentRequest.getSecret()));
 		}
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(failedContentRequest.getHubURL())
 				.queryParam("topic", failedContentRequest.getTopic())
@@ -184,7 +185,7 @@ public class SubscriberClientImpl
 						Base64.encodeBase64URLSafeString(failedContentRequest.getCallbackURL().getBytes()))
 				.queryParam("timestamp", failedContentRequest.getTimestamp())
 				.queryParam("pageindex",
-						failedContentRequest.getPaginationIndex() < 0 ? null : failedContentRequest.getPaginationIndex())
+						pageIndex)
 				.queryParam("messageCount",
 						failedContentRequest.getMessageCount() <= 0 ? null : failedContentRequest.getMessageCount());
 
