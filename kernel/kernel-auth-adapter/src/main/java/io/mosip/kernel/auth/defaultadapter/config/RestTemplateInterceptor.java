cdfaff221c7fa1258a54294d41dca4c506523ef7
Urvil Joshi <42868205+urviljoshi@users.noreply.github.com>
2021-06-24 21:34:50 +0530

    Feature MOSIP-11421 (#868)
    * MOSIP-14269 added two interceptor for two behaviours
    
    * added default rest interceptor to the plain rest template
    
    * MOSIP-14269 added background job and other optimizations
    
    * MOSIP-14269 added option to skip reused token
    
    * MOSIP-14269 add option to skip token reuse

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/RestTemplateInterceptor.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/RestTemplateInterceptor.java
@@ -52,47 +52,18 @@ public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
 	@Autowired(required = false)
 	private LoadBalancerClient loadBalancerClient;
 
-	// This config is introduced as a patch to fix the use of expired token from security context 
-	// rather than taking token from actual request. Later proper default behaviour to be 
-	// coded and this config to be removed
-	@Value("${auth.adapter.rest.template.skip.context.token.reuse:false}")
-	private boolean skipContextTokenReuse;
 
 	@Override
 	public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes,
 			ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
-		addHeadersToRequest(httpRequest, bytes);
+		
 		httpRequest = resolveServiceId(httpRequest);
 		ClientHttpResponse response = clientHttpRequestExecution.execute(httpRequest, bytes);
-		// getHeadersFromResponse(response);
 		return response;
 	}
 
-	private void addHeadersToRequest(HttpRequest httpRequest, byte[] bytes) {
-		HttpHeaders headers = httpRequest.getHeaders();
-		if(!skipContextTokenReuse) {
-			AuthUserDetails authUserDetails = getAuthUserDetails();
-			if (authUserDetails != null)
-				headers.set(AuthAdapterConstant.AUTH_HEADER_COOKIE,
-						AuthAdapterConstant.AUTH_COOOKIE_HEADER + authUserDetails.getToken());
-		}
-	}
 
-	private AuthUserDetails getAuthUserDetails() {
-		AuthUserDetails authUserDetails = null;
-		if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null
-				&& SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof AuthUserDetails)
-
-			authUserDetails = (AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		return authUserDetails;
-	}
-
-	private void getHeadersFromResponse(ClientHttpResponse clientHttpResponse) {
-		HttpHeaders headers = clientHttpResponse.getHeaders();
-		String responseToken = headers.get(AuthAdapterConstant.AUTH_HEADER_SET_COOKIE).get(0)
-				.replaceAll(AuthAdapterConstant.AUTH_COOOKIE_HEADER, "");
-		getAuthUserDetails().setToken(responseToken);
-	}
+	
 
 	private HttpRequest resolveServiceId(HttpRequest request) {
 		try {
@@ -107,8 +78,7 @@ public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
 				}
 			}
 		} catch (Exception ex) {
-			LOGGER.warn("Failed to choose service instance : " + ex.getMessage());
-			LOGGER.debug("Failed to choose service instance", ex);
+			LOGGER.warn("Failed to choose service instance : {}",ex.getMessage());
 		}
 		return request;
 	}
