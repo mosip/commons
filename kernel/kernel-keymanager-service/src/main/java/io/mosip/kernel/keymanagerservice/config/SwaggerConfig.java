e0b0bd646be5295eebaa2293716aa4ea4f472b93
kameshsr <kameshsr1338@gmail.com>
2021-06-30 17:27:51 +0530

    MOSIP-15448: Generated openapi.json for Commons - keymanager Service

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/config/SwaggerConfig.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/config/SwaggerConfig.java
@@ -95,7 +95,7 @@ public class SwaggerConfig {
 
 		Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
 				.tags(new Tag("keymanager", "Operations related to keymanagement and interaction with Softhsm/real HSM for Key Generation."))
-				.groupName(TITLE).select().apis(RequestHandlerSelectors.any())
+				.select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.regex("(?!/(error).*).*")).build();
 
 		if (swaggerBaseUrlSet) {
