a6d6631f46a1bc2888ba79b3593f1b50979b5494
urviljoshi <urvvil08@gmail.com>
2021-07-08 16:09:42 +0530

    added toggle on restTemplate bean on property

--- kernel/kernel-websubclient-api/src/main/java/io/mosip/kernel/websub/api/config/WebSubClientConfig.java
+++ kernel/kernel-websubclient-api/src/main/java/io/mosip/kernel/websub/api/config/WebSubClientConfig.java
@@ -3,6 +3,8 @@ package io.mosip.kernel.websub.api.config;
 import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -52,7 +54,9 @@ public class WebSubClientConfig {
 
 	@Bean(name = "websubRestTemplate")
 	public RestTemplate restTemplate() {
+
 		return new RestTemplate();
+
 	}
 
 	@Bean(name = "intentVerificationFilterBean")
