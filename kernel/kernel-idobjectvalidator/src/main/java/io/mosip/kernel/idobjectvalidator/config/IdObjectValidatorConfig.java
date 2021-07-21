f5d61cb1d191031aaccb9ff4c44fbe175761cc77
Manoj SP <Manoj.SP@mindtree.com>
2021-07-21 12:12:03 +0530

    resolved conflicts

--- kernel/kernel-idobjectvalidator/src/main/java/io/mosip/kernel/idobjectvalidator/config/IdObjectValidatorConfig.java
+++ kernel/kernel-idobjectvalidator/src/main/java/io/mosip/kernel/idobjectvalidator/config/IdObjectValidatorConfig.java
@@ -2,7 +2,6 @@ package io.mosip.kernel.idobjectvalidator.config;
 
 import static io.mosip.kernel.idobjectvalidator.constant.IdObjectValidatorConstant.REFERENCE_VALIDATOR;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +10,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -30,9 +28,6 @@ import io.mosip.kernel.core.idobjectvalidator.spi.IdObjectValidator;
 @Configuration
 public class IdObjectValidatorConfig {
 
-	@Value("${mosip.kernel.idobjectvalidator.enabled:true}")
-	private boolean isEnabled;
-
 	private static final Logger logger = LoggerFactory.getLogger(IdObjectValidatorConfig.class);
 
 	/** The env. */
@@ -46,7 +41,7 @@ public class IdObjectValidatorConfig {
 	 */
 	@PostConstruct
 	public void validateReferenceValidator() throws ClassNotFoundException {
-		if (isEnabled && StringUtils.isNotBlank(env.getProperty(REFERENCE_VALIDATOR))) {
+		if (StringUtils.isNotBlank(env.getProperty(REFERENCE_VALIDATOR))) {
 			logger.debug("validating referenceValidator Class is present or not");
 			Class.forName(env.getProperty(REFERENCE_VALIDATOR));
 		}
@@ -57,27 +52,24 @@ public class IdObjectValidatorConfig {
 	 * Reference validator.
 	 *
 	 * @return the id object validator
-	 * @throws ClassNotFoundException    the class not found exception
-	 * @throws InstantiationException    the instantiation exception
-	 * @throws IllegalAccessException    the illegal access exception
-	 * @throws SecurityException
-	 * @throws NoSuchMethodException
-	 * @throws InvocationTargetException
-	 * @throws IllegalArgumentException
+	 * @throws ClassNotFoundException the class not found exception
+	 * @throws InstantiationException the instantiation exception
+	 * @throws IllegalAccessException the illegal access exception
 	 */
 	@Bean
 	@Lazy
-	public IdObjectValidator referenceValidator() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
-			IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		if (isEnabled && StringUtils.isNotBlank(env.getProperty(REFERENCE_VALIDATOR))) {
+	public IdObjectValidator referenceValidator()
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		if (StringUtils.isNotBlank(env.getProperty(REFERENCE_VALIDATOR))) {
 			logger.debug("instance of referenceValidator is created");
-			return (IdObjectValidator) Class.forName(env.getProperty(REFERENCE_VALIDATOR)).getDeclaredConstructor().newInstance();
+			return (IdObjectValidator) Class.forName(env.getProperty(REFERENCE_VALIDATOR)).newInstance();
 		} else {
 			logger.debug("no reference validator is provided");
 			return new IdObjectValidator() {
 
 				@Override
-				public boolean validateIdObject(String identitySchema, Object identityObject, List<String> requiredFields)
+				public boolean validateIdObject(String identitySchema, Object identityObject,
+						List<String> requiredFields)
 						throws IdObjectValidationFailedException, IdObjectIOException, InvalidIdSchemaException {
 					return true;
 				}
