a4e8e802d0b3d93aa440e195181ae0a8c2412d40
SatishG_Neosoft <NEOSOFT_SatishG@nsdl.co.in>
2021-08-02 15:52:46 +0530

    resolved comments

--- kernel/kernel-biometrics-api/src/main/java/io/mosip/kernel/biometrics/commons/BiometricsSignatureHelper.java
+++ kernel/kernel-biometrics-api/src/main/java/io/mosip/kernel/biometrics/commons/BiometricsSignatureHelper.java
@@ -3,6 +3,7 @@ package io.mosip.kernel.biometrics.commons;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,16 +19,22 @@ public class BiometricsSignatureHelper {
 	public static String extractJWTToken(BIR bir) throws BiometricSignatureValidationException, JSONException {
 
 		String constructedJWTToken = null;
+		
+		List<Entry> othersInfo = bir.getOthers();
+		if (othersInfo == null) {
+			throw new BiometricSignatureValidationException("Others value is null inside BIR");
+		}
+
 		String sb = new String(bir.getSb(), StandardCharsets.UTF_8);
 		String bdb = Base64.getUrlEncoder().encodeToString(bir.getBdb());
 
-		for (Entry entry : bir.getOthers()) {
+		for (Entry entry : othersInfo) {
 			if (entry.getKey().equals("PAYLOAD")) {
 				String value = entry.getValue().replace("<bioValue>", bdb);
 				String encodedPayloadValue = CryptoUtil.encodeBase64(value.getBytes());
 				constructedJWTToken = constructJWTToken(sb, encodedPayloadValue);
 				JSONObject jsonObject = new JSONObject(value);
-				String digitalID = jsonObject.getString("digitalId").split("\\.")[0];
+				String digitalID = jsonObject.getString("digitalId");
 				compareJWTForSameCertificates(constructedJWTToken, digitalID);
 			}
 		}
@@ -35,29 +42,29 @@ public class BiometricsSignatureHelper {
 
 	}
 
-	private static void compareJWTForSameCertificates(String constructedJWTToken, String digitalID)
+	private static void compareJWTForSameCertificates(String jwtString1, String jwtString2)
 			throws JSONException, BiometricSignatureValidationException {
-		String digitalIdHeader = new String(CryptoUtil.decodeBase64(digitalID));
-		JSONObject digitalHeaderCertificate = new JSONObject(digitalIdHeader);
-		JSONArray digitalHeadercertificateJsonArray = digitalHeaderCertificate.getJSONArray("x5c");
-		ArrayList<String> digitalCertificates = new ArrayList<String>();
-		if (digitalHeadercertificateJsonArray != null) {
-			for (int i = 0; i < digitalHeadercertificateJsonArray.length(); i++) {
-				digitalCertificates.add(digitalHeadercertificateJsonArray.getString(i));
+		String jwtString1Header = new String(CryptoUtil.decodeBase64(jwtString1.split("\\.")[0]));
+		JSONObject jwtString1HeaderCertificate = new JSONObject(jwtString1Header);
+		JSONArray jwtString1HeadercertificateJsonArray = jwtString1HeaderCertificate.getJSONArray("x5c");
+		ArrayList<String> jwtString1Certificates = new ArrayList<String>();
+		if (jwtString1HeadercertificateJsonArray != null) {
+			for (int i = 0; i < jwtString1HeadercertificateJsonArray.length(); i++) {
+				jwtString1Certificates.add(jwtString1HeadercertificateJsonArray.getString(i));
 			}
 		}
 
-		String sbHeader = new String(CryptoUtil.decodeBase64(constructedJWTToken.split("\\.")[0]));
-		JSONObject sbHeaderCertificate = new JSONObject(sbHeader);
-		JSONArray sbHeadercertificateJsonArray = sbHeaderCertificate.getJSONArray("x5c");
-		ArrayList<String> sbCertificates = new ArrayList<String>();
-		if (sbHeadercertificateJsonArray != null) {
-			for (int i = 0; i < sbHeadercertificateJsonArray.length(); i++) {
-				sbCertificates.add(sbHeadercertificateJsonArray.getString(i));
+		String jwtString2Header = new String(CryptoUtil.decodeBase64(jwtString2.split("\\.")[0]));
+		JSONObject jwtString2HeaderCertificate = new JSONObject(jwtString2Header);
+		JSONArray jwtString2HeadercertificateJsonArray = jwtString2HeaderCertificate.getJSONArray("x5c");
+		ArrayList<String> jwtString2Certificates = new ArrayList<String>();
+		if (jwtString2HeadercertificateJsonArray != null) {
+			for (int i = 0; i < jwtString2HeadercertificateJsonArray.length(); i++) {
+				jwtString2Certificates.add(jwtString2HeadercertificateJsonArray.getString(i));
 			}
 		}
 
-		if (!digitalCertificates.containsAll(sbCertificates)) {
+		if (!jwtString1Certificates.containsAll(jwtString2Certificates)) {
 			throw new BiometricSignatureValidationException("Header Certificate mismatch");
 		}
 	}
