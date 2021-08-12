f18776b947e20a3c3fb559396f8b4beefd3eb140
ase-101 <>
2021-08-12 15:09:28 +0530

    Updated to pass age groups

--- kernel/kernel-applicanttype-api/src/main/java/io/mosip/kernel/applicanttype/api/impl/ApplicantTypeImpl.java
+++ kernel/kernel-applicanttype-api/src/main/java/io/mosip/kernel/applicanttype/api/impl/ApplicantTypeImpl.java
@@ -1,10 +1,14 @@
 package io.mosip.kernel.applicanttype.api.impl;
 
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mvel2.MVEL;
 import org.mvel2.integration.VariableResolverFactory;
 import org.mvel2.integration.impl.MapVariableResolverFactory;
@@ -30,67 +34,62 @@ public class ApplicantTypeImpl implements ApplicantType {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicantTypeImpl.class);
 
-	@Value("${mosip.kernel.applicant.type.age.limit}")
-	private String ageLimit;
+	@Value("${mosip.regproc.packet.classifier.tagging.agegroup.ranges:{'INFANT':'0-5','MINOR':'6-17','ADULT':'18-200'}}")
+	private String ageGroups;
 
-	@Value("${mosip.kernel.config.server.file.storage.uri}")
+	@Value("${mosip.kernel.config.server.file.storage.uri:https://localhost/config/}")
 	private String configServerFileStorageURL;
 
-	@Value("${mosip.kernel.applicantType.mvel.file}")
+	@Value("${mosip.kernel.applicantType.mvel.file:applicanttype.mvel}")
 	private String mvelFile;
 
 	@Autowired
 	private RestTemplate restTemplate;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * io.mosip.kernel.core.applicanttype.spi.ApplicantCodeService#getApplicantType(
-	 * java.util.Map)
+	private ObjectMapper objectMapper = new ObjectMapper();
+	private String SCRIPT = null;
+
+	private String getScript() {
+		synchronized (SCRIPT) {
+			if(SCRIPT == null)
+				SCRIPT = restTemplate.getForObject(configServerFileStorageURL+mvelFile, String.class);
+		}
+		return SCRIPT;
+	}
+
+	/**
+	 *
+	 * @param map contains attribute and its value
+	 * @return
+	 * @throws InvalidApplicantArgumentException
 	 */
-	public String getApplicantType(Map<String, Object> m) throws InvalidApplicantArgumentException {
-		LOGGER.info("Getting code for applicant type");
+	@Override
+	public String getApplicantType(Map<String, Object> map) throws InvalidApplicantArgumentException {
+		Map<String, Object> context = new HashMap();
 		try {
-			LOGGER.info("Getting data from MVEL file->"+configServerFileStorageURL + mvelFile);
-			String mvelExpression = restTemplate.getForObject(configServerFileStorageURL + mvelFile, String.class);
+			Map<String, String> ageGroupsMap = objectMapper.readValue(ageGroups,
+					new TypeReference<HashMap<String, String>>(){});
+			context.put("age-groups", ageGroupsMap);
+		} catch (IOException e) {
+			LOGGER.error("Failed to parse age groups configuration", e);
+		}
 
-			Map<String, Object> context = new HashMap();
+		MVEL.eval(getScript(), context);
+		context.put("identity", map);
+		final String code =  MVEL.eval("return getApplicantType();", context, String.class);
+		LOGGER.info("Evaluated applicant code : {}", code);
 
-			context.put("map", m);
-			context.put("agelimit", ageLimit == null ? "18" : ageLimit);
-
-			VariableResolverFactory functionFactory = new MapVariableResolverFactory();
-			MVEL.eval(mvelExpression, context, functionFactory);
-
-			VariableResolverFactory myVarFactory = new MapVariableResolverFactory();
-			myVarFactory.setNextFactory(functionFactory);
-
-			Serializable s = MVEL.compileExpression("getApplicantType(map,agelimit);", context);
-
-			String code =  MVEL.executeExpression(s, m, myVarFactory).toString();
-			
-			if(code.equalsIgnoreCase("KER-MSD-151"))
-			{
-				LOGGER.error("Error in provided DOB");
+		switch (code) {
+			case "KER-MSD-151":
 				throw new InvalidApplicantArgumentException(
 						ApplicantTypeErrorCode.INVALID_DATE_DOB_EXCEED_EXCEPTION.getErrorCode(),
 						ApplicantTypeErrorCode.INVALID_DATE_DOB_EXCEED_EXCEPTION.getErrorMessage());
-				
-			}
-			else if (code.equalsIgnoreCase("KER-MSD-147")) {
-				LOGGER.error("Error while fetching applicant code");
+			case "KER-MSD-147":
 				throw new InvalidApplicantArgumentException(
 						ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorCode(),
 						ApplicantTypeErrorCode.INVALID_QUERY_EXCEPTION.getErrorMessage());
-			}
-			LOGGER.info("Code for applicant type  is " + code);
-			return code;
-		} catch (Exception ex) {
-			LOGGER.error("Error while executing mvel file" + ex.getMessage());
-			throw ex;
+			default:
+				return code;
 		}
-		
 	}
-
 }
