025bbff994fcdbcd036db38cc4d556edef768f7d
Urvil Joshi <42868205+urviljoshi@users.noreply.github.com>
2021-09-20 14:01:04 +0530

    MOSIP-17298 depricating old methods and adding new ones. (#1019)
    * MOSIP-17298 depricating old methods and adding new ones.
    
    * MOSIP-17298 Review changes added

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/CryptoUtil.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/CryptoUtil.java
@@ -70,6 +70,7 @@ public class CryptoUtil {
 	 * @param data data to encode
 	 * @return encoded data
 	 */
+	@Deprecated(since = "1.1.5.5", forRemoval = true)
 	public static String encodeBase64(byte[] data) {
 		return Base64.getUrlEncoder().encodeToString(data);
 	}
@@ -80,6 +81,7 @@ public class CryptoUtil {
 	 * @param data data to encode
 	 * @return encoded data
 	 */
+	@Deprecated(since = "1.1.5.5", forRemoval = true)
 	public static String encodeBase64String(byte[] data) {
 		return Base64.getEncoder().encodeToString(data);
 	}
@@ -90,8 +92,34 @@ public class CryptoUtil {
 	 * @param data data to decode
 	 * @return decoded data
 	 */
+	/*
+	 * This impl was a upgrade from apache coded to java 8 as apache has a single
+	 * decoder for decoding both url safe and standard base64 encoding but java 8
+	 * has two decoders we are follwing this approach.
+	 */
+	@Deprecated(since = "1.1.5.5", forRemoval = true)
 	public static byte[] decodeBase64(String data) {
+		try {
+			return Base64.getUrlDecoder().decode(data.getBytes());
+		} catch (IllegalArgumentException exception) {
+			return Base64.getDecoder().decode(data.getBytes());
+		}
+	}
+
+	public static String encodeToURLSafeBase64(byte[] data) {
+		return Base64.getUrlEncoder().encodeToString(data);
+	}
+
+	public static byte[] decodeURLSafeBase64(String data) {
 		return Base64.getUrlDecoder().decode(data.getBytes());
+	}
+
+	public static String encodeToPlainBase64(byte[] data) {
+		return Base64.getEncoder().encodeToString(data);
+	}
+
+	public static byte[] decodePlainBase64(String data) {
+		return Base64.getDecoder().decode(data.getBytes());
 	}
 
 	/**
