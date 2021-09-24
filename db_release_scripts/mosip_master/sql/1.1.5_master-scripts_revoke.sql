5ed2f51465daf3accf59f4d4d4f35fa69b6bb328
ckm007 <chandrakeshavmishra@gmail.com>
2021-09-24 06:05:13 +0000

    [DSD-390] updated the DB release scripts for mosip_master 1.1.5

--- db_release_scripts/mosip_master/sql/1.1.5_master-scripts_revoke.sql
+++ db_release_scripts/mosip_master/sql/1.1.5_master-scripts_revoke.sql
@@ -5,17 +5,14 @@
 -- Create By   		: Ram Bhatt
 -- Created Date		: Apr-2021
 -- 
--- Modified Date        Modified By         Comments / Remarks
+-- Modified Date        Modified By         	Comments / Remarks
+-- Sept-2021		Chandra Keshav Mishra	Removed the TRUNCATE statements from the revoke scripts.
+--						Also as part of 1.1.5 revoke script noting is required as we are just adding a table there in 1.1.5 and if downgrade is done there is no harm in keeping the table intact there.
+--
 -- -----------------------------------------------------------------------------------------------------------
 
 -- -----------------------------------------------------------------------------------------------------------
 \c mosip_master sysadmin
 
-TRUNCATE TABLE master.dynamic_field cascade ;
 
-\COPY master.dynamic_field (id,name,description,data_type,value_json,lang_code,is_active,cr_by,cr_dtimes) FROM '../dml/master-dynamic_field.csv' delimiter ',' HEADER  csv;
-
-TRUNCATE TABLE master.identity_schema cascade ;
-
-\COPY master.identity_schema (id,id_version,title,description,id_attr_json,schema_json,status_code,add_props,effective_from,lang_code,is_active,cr_by,cr_dtimes) FROM '../dml/master-identity_schema.csv' delimiter ',' HEADER  csv;
 --------------------------------------------------------------------------------------------------------------
