da0b0cefa8277f28e8b0fef9e81d7ee23e12412f
urviljoshi <urvvil08@gmail.com>
2021-09-30 16:56:40 +0530

    addin without padding to encoder

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/CryptoUtil.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/CryptoUtil.java
@@ -3,6 +3,7 @@ package io.mosip.kernel.core.util;
 import static java.util.Arrays.copyOfRange;
 
 import java.util.Base64;
+import java.util.Base64.Encoder;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.ArrayUtils;
@@ -15,6 +16,13 @@ import org.apache.commons.lang3.ArrayUtils;
  * @since 1.0.0
  */
 public class CryptoUtil {
+	
+	private static Encoder urlSafeEncoder;
+	
+	
+	static {
+		urlSafeEncoder = Base64.getUrlEncoder().withoutPadding();
+	}
 
 	/**
 	 * Private Constructor for this class
@@ -72,7 +80,7 @@ public class CryptoUtil {
 	 */
 	@Deprecated(since = "1.1.5.5", forRemoval = true)
 	public static String encodeBase64(byte[] data) {
-		return Base64.getUrlEncoder().encodeToString(data);
+		return urlSafeEncoder.encodeToString(data);
 	}
 
 	/**
@@ -113,7 +121,7 @@ public class CryptoUtil {
 		if (EmptyCheckUtils.isNullEmpty(data)) {
 			return null;
 		}
-		return Base64.getUrlEncoder().encodeToString(data);
+		return urlSafeEncoder.encodeToString(data);
 	}
 
 	public static byte[] decodeURLSafeBase64(String data) {
