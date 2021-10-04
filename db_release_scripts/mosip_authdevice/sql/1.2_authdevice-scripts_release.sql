2a4e3f6f8ccb83c2592fb63a5dc97c1a0cb3bbeb
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-10-04 10:51:09 +0530

    MOSIP-15061 Increase column length

---
+++ db_release_scripts/mosip_authdevice/sql/1.2_authdevice-scripts_release.sql
@@ -0,0 +1,19 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name	: mosip_authdevice
+-- Release Version 	: 1.2.0-SNAPSHOT
+-- Purpose    		: Database Alter scripts for the release for Authdevice DB.       
+-- Create By   		: Ram Bhatt
+-- Created Date		: Oct-2021
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+--
+-- -------------------------------------------------------------------------------------------------
+
+\c mosip_authdevice sysadmin
+
+-- -------------------------------------------------------------------------------------------------
+
+ALTER TABLE authdevice.secure_biometric_interface ALTER COLUMN device_detail_id TYPE character varying;
+ALTER TABLE authdevice.secure_biometric_interface_h ALTER COLUMN device_detail_id TYPE character varying;
+
+----------------------------------------------------------------------------------------------------
---
+++ db_release_scripts/mosip_regdevice/sql/1.2_regdevice-scripts_release.sql
@@ -0,0 +1,17 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name	: mosip_regdevice
+-- Release Version 	: 1.2.0-SNAPSHOT
+-- Purpose    		: Database Alter scripts for the release for Regdevice DB.       
+-- Create By   		: Ram Bhatt
+-- Created Date		: Oct-2021
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -------------------------------------------------------------------------------------------------
+
+\c mosip_regdevice sysadmin
+
+ALTER TABLE regdevice.secure_biometric_interface ALTER COLUMN device_detail_id TYPE character varying;
+ALTER TABLE regdevice.secure_biometric_interface_h ALTER COLUMN device_detail_id TYPE character varying;
+----------------------------------------------------------------------------------------------------
+
+
