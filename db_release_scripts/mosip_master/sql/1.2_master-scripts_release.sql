21eaf260a9e79aa247ec39d187ba400152adda8c
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-11-10 14:05:16 +0530

    Path change of ui_spec csv

--- db_release_scripts/mosip_master/sql/1.2_master-scripts_release.sql
+++ db_release_scripts/mosip_master/sql/1.2_master-scripts_release.sql
@@ -40,7 +40,7 @@ TRUNCATE TABLE master.blocklisted_words cascade ;
 TRUNCATE TABLE master.ui_spec  cascade ;
 
 ---------------------------------------------------------------------------------------------------------------------
-\COPY master.ui_spec (ID, VERSION , DOMAIN, TITLE, DESCRIPTION, TYPE, IDENTITY_SCHEMA_ID, IDENTITY_SCHEMA_VERSION, JSON_SPEC, STATUS_CODE, EFFECTIVE_FROM , ADD_PROPS, IS_ACTIVE , CR_BY, CR_DTIMES, UPD_BY, UPD_DTIMES, IS_DELETED,DEL_DTIMES) FROM '../dml/master-ui_spec.csv' delimiter ',' HEADER  csv;
+\COPY master.ui_spec (ID, VERSION , DOMAIN, TITLE, DESCRIPTION, TYPE, IDENTITY_SCHEMA_ID, IDENTITY_SCHEMA_VERSION, JSON_SPEC, STATUS_CODE, EFFECTIVE_FROM , ADD_PROPS, IS_ACTIVE , CR_BY, CR_DTIMES, UPD_BY, UPD_DTIMES, IS_DELETED,DEL_DTIMES) FROM './dml/master-ui_spec.csv' delimiter ',' HEADER  csv;
 -----------------------------------------------------DATA LOAD FROM IDENTITY SCHEMA TABLE-----------------------------------------------
 INSERT into master.ui_spec (id,version,domain,title,description,type,json_spec,identity_schema_id,identity_schema_version,effective_from,status_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes) SELECT id,id_version,'registration-client', title,description,'schema',id_attr_json,id,id_version,effective_from,status_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes FROM master.identity_schema;
 
