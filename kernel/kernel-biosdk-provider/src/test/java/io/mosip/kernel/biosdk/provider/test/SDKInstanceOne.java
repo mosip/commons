32123b836549cfef7c06534b9627ddcd18c7a162
Urvil Joshi <42868205+urviljoshi@users.noreply.github.com>
2021-11-30 13:05:17 +0530

    jnunit added for bio sdk provider (#1089)
    * jnunit added for bio sdk provider
    
    * jnunit added for bio sdk provider

--- kernel/kernel-biosdk-provider/src/test/java/io/mosip/kernel/biosdk/provider/test/SDKInstanceOne.java
+++ kernel/kernel-biosdk-provider/src/test/java/io/mosip/kernel/biosdk/provider/test/SDKInstanceOne.java
@@ -2,24 +2,28 @@ package io.mosip.kernel.biosdk.provider.test;
 
 import io.mosip.kernel.biometrics.constant.BiometricFunction;
 import io.mosip.kernel.biometrics.constant.BiometricType;
+import io.mosip.kernel.biometrics.constant.Match;
 import io.mosip.kernel.biometrics.entities.BiometricRecord;
+import io.mosip.kernel.biometrics.model.Decision;
 import io.mosip.kernel.biometrics.model.MatchDecision;
 import io.mosip.kernel.biometrics.model.QualityCheck;
+import io.mosip.kernel.biometrics.model.QualityScore;
 import io.mosip.kernel.biometrics.model.Response;
 import io.mosip.kernel.biometrics.model.SDKInfo;
 import io.mosip.kernel.biometrics.spi.IBioApi;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TestSDK2 implements IBioApi {
+public class SDKInstanceOne implements IBioApi {
 
     @Override
     public SDKInfo init(Map<String, String> initParams) {
-        SDKInfo sdkInfo = new SDKInfo("0.9", "1", "MOCKVendor2", "test2");
-        sdkInfo.withSupportedMethod(BiometricFunction.MATCH, BiometricType.IRIS);
-        sdkInfo.withSupportedMethod(BiometricFunction.EXTRACT, BiometricType.IRIS);
-        sdkInfo.withSupportedMethod(BiometricFunction.QUALITY_CHECK, BiometricType.IRIS);
+        SDKInfo sdkInfo = new SDKInfo("0.9", "1.0", "MOCKVendor1", "test");
+        sdkInfo.withSupportedMethod(BiometricFunction.MATCH, BiometricType.FINGER);
+        sdkInfo.withSupportedMethod(BiometricFunction.EXTRACT, BiometricType.FINGER);
+        sdkInfo.withSupportedMethod(BiometricFunction.QUALITY_CHECK, BiometricType.FINGER);
 
         sdkInfo.withSupportedMethod(BiometricFunction.MATCH, BiometricType.FACE);
         sdkInfo.withSupportedMethod(BiometricFunction.EXTRACT, BiometricType.FACE);
@@ -29,17 +33,40 @@ public class TestSDK2 implements IBioApi {
 
     @Override
     public Response<QualityCheck> checkQuality(BiometricRecord sample, List<BiometricType> modalitiesToCheck, Map<String, String> flags) {
-        return null;
+    	 Response<QualityCheck> response = new Response<>();
+    	 QualityCheck qualityCheck = new QualityCheck();
+    	 Map<BiometricType, QualityScore> scores = new HashMap<>();
+    	 QualityScore qualityScore= new QualityScore();
+    	 qualityScore.setScore(90.0F);
+    	 scores.put(BiometricType.FINGER,qualityScore);
+    	 qualityCheck.setScores(scores);
+    	 response.setStatusCode(210);
+    	 response.setResponse(qualityCheck);
+    	return response;
     }
 
     @Override
     public Response<MatchDecision[]> match(BiometricRecord sample, BiometricRecord[] gallery, List<BiometricType> modalitiesToMatch, Map<String, String> flags) {
-        return null;
+        Response<MatchDecision[]> response = new Response<>();
+        MatchDecision matchDecision = new MatchDecision(0);
+        Map<BiometricType, Decision> decisions = new HashMap<>();
+        Decision decision = new Decision();
+        decision.setMatch(Match.MATCHED);
+        decisions.put(BiometricType.FINGER, decision);
+        matchDecision.setDecisions(decisions);
+        MatchDecision[] matchDecisions = new MatchDecision[1];
+        matchDecisions[0]=matchDecision;
+        response.setStatusCode(210);
+        response.setResponse(matchDecisions);
+        return response;
     }
 
     @Override
     public Response<BiometricRecord> extractTemplate(BiometricRecord sample, List<BiometricType> modalitiesToExtract, Map<String, String> flags) {
-        return null;
+    	 Response<BiometricRecord> response = new Response<>();
+    	 response.setStatusCode(210);
+         response.setResponse(sample);
+    	 return response;
     }
 
     @Override
