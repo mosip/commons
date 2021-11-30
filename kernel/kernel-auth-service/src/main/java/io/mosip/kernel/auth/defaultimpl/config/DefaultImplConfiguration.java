d944e33ab0aae9c2b9f615c5f8acb678b4c136ea
urviljoshi <urvvil08@gmail.com>
2021-11-30 13:44:27 +0530

    merging MOSIP-17980 resolution to 1.1.5.5

--- kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/defaultimpl/config/DefaultImplConfiguration.java
+++ kernel/kernel-auth-service/src/main/java/io/mosip/kernel/auth/defaultimpl/config/DefaultImplConfiguration.java
@@ -1,8 +1,7 @@
 package io.mosip.kernel.auth.defaultimpl.config;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import io.mosip.kernel.auth.defaultimpl.dto.AccessTokenResponse;
 import io.mosip.kernel.auth.defaultimpl.intercepter.RestInterceptor;
 import io.mosip.kernel.auth.defaultimpl.util.MemoryCache;
+import io.mosip.kernel.auth.defaultimpl.util.TokenValidator;
 
 @Configuration
 public class DefaultImplConfiguration {
@@ -25,5 +25,11 @@ public class DefaultImplConfiguration {
 	@Bean
 	public MemoryCache<String, AccessTokenResponse> memoryCache() {
 		return new MemoryCache<>(1);
+	}
+	
+
+	@Bean
+	public RestInterceptor restInterceptor(@Autowired  MemoryCache<String, AccessTokenResponse> memoryCache,@Autowired TokenValidator tokenValidator,@Qualifier("authRestTemplate") @Autowired RestTemplate restTemplate) {
+		return new RestInterceptor(memoryCache,tokenValidator,restTemplate);
 	}
 }
