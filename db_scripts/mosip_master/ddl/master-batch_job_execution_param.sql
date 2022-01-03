9d51f652afb4d4657f45ede932aabea92dedc469
dhanendra06 <dhanendra.tech@gmail.com>
2022-01-03 13:05:03 +0530

    modified constraint for blocklisted_words table

--- db_scripts/mosip_master/ddl/master-batch_job_execution_param.sql
+++ db_scripts/mosip_master/ddl/master-batch_job_execution_param.sql
@@ -7,7 +7,7 @@ CREATE TABLE master.batch_job_execution_params
     job_execution_id bigint NOT NULL,
     type_cd character varying(6) COLLATE pg_catalog."default" NOT NULL,
     key_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    string_val character varying(250) COLLATE pg_catalog."default",
+    string_val character varying(5000) COLLATE pg_catalog."default",
     date_val timestamp without time zone,
     long_val bigint,
     double_val double precision,
