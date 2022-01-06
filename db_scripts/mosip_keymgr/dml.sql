a2895c1882459bb576b57fb504f3287b42761ff9
Puneet Joshi <puneet@mosip.io>
2022-01-06 17:30:32 +0530

    [MOSIP-17836] DML corrected.

--- db_scripts/mosip_keymgr/dml.sql
+++ db_scripts/mosip_keymgr/dml.sql
@@ -2,7 +2,7 @@
 
 TRUNCATE TABLE keymgr.key_policy_def cascade ;
 
-\COPY keymgr.key_policy_def (app_id,key_validity_duration,is_active,cr_by,cr_dtimes) FROM './dml/keymgr-key_policy_def.csv' delimiter ',' HEADER  csv;
+\COPY keymgr.key_policy_def (app_id,key_validity_duration,is_active,cr_by,cr_dtimes,pre_expire_days,access_allowed) FROM './dml/keymgr-key_policy_def.csv' delimiter ',' HEADER  csv;
 
 TRUNCATE TABLE keymgr.key_policy_def_h cascade ;
 
