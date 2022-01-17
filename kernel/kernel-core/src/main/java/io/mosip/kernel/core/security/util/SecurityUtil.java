c9fe198e90b0709745cd5e636eabb087b78e4f5c
Mahammed Taheer <mohd.taheer@gmail.com>
2022-01-17 15:24:40 +0530

    Remove Keymanager dependency from id generator components.

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/security/util/SecurityUtil.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/security/util/SecurityUtil.java
@@ -8,6 +8,17 @@
 package io.mosip.kernel.core.security.util;
 
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.SecureRandom;
+import java.util.Objects;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.util.PrivateKeyFactory;
@@ -16,6 +27,8 @@ import org.bouncycastle.crypto.util.PublicKeyFactory;
 import io.mosip.kernel.core.security.constants.MosipSecurityExceptionCodeConstants;
 import io.mosip.kernel.core.security.constants.MosipSecurityMethod;
 import io.mosip.kernel.core.security.exception.MosipInvalidKeyException;
+import io.mosip.kernel.core.security.exception.MosipNoSuchAlgorithmException;
+import io.mosip.kernel.core.security.exception.MosipNullDataException;
 import io.mosip.kernel.core.security.exception.MosipNullKeyException;
 import io.mosip.kernel.core.security.exception.MosipNullMethodException;
 
@@ -26,6 +39,14 @@ import io.mosip.kernel.core.security.exception.MosipNullMethodException;
  * @since 1.0.0
  */
 public class SecurityUtil {
+
+	private static final String SYMMETRIC_ALGORITHM = "AES/GCM/PKCS5Padding";
+
+	private static final String AES = "AES";
+
+	private static final int TAG_LENGTH = 128;
+
+	private static SecureRandom secureRandom;
 
 	/**
 	 * Constructor for this class
@@ -87,5 +108,51 @@ public class SecurityUtil {
 		if (mosipSecurityMethod == null) {
 			throw new MosipNullMethodException(MosipSecurityExceptionCodeConstants.MOSIP_NULL_METHOD_EXCEPTION);
 		}
+	}
+
+	// Added below method for temporarily to fix the build issue causing cross dependency between core & keymanager service.
+	public static byte[] symmetricEncrypt(SecretKey key, byte[] data) {
+		Objects.requireNonNull(key, MosipSecurityExceptionCodeConstants.MOSIP_INVALID_KEY_EXCEPTION.getErrorMessage());
+		if (Objects.isNull(data) || data.length == 0) {
+			throw new MosipNullDataException(MosipSecurityExceptionCodeConstants.MOSIP_INVALID_DATA_EXCEPTION);
+		}
+
+		Cipher cipher;
+		try {
+			cipher = Cipher.getInstance(SYMMETRIC_ALGORITHM);
+		} catch (java.security.NoSuchAlgorithmException | NoSuchPaddingException e) {
+			throw new MosipNoSuchAlgorithmException(
+				MosipSecurityExceptionCodeConstants.MOSIP_NO_SUCH_ALGORITHM_EXCEPTION);
+		}
+		try {
+			byte[] output = null;
+			byte[] randomIV = generateIV(cipher.getBlockSize());
+			SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), AES);
+			GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(TAG_LENGTH, randomIV);
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmParameterSpec);
+			output = new byte[cipher.getOutputSize(data.length) + cipher.getBlockSize()];
+			byte[] processData = cipher.doFinal(data);
+			System.arraycopy(processData, 0, output, 0, processData.length);
+			System.arraycopy(randomIV, 0, output, processData.length, randomIV.length);
+			return output;
+		} catch (java.security.InvalidKeyException e) {
+			throw new MosipInvalidKeyException(MosipSecurityExceptionCodeConstants.MOSIP_INVALID_KEY_EXCEPTION);
+		} catch (InvalidAlgorithmParameterException e) {
+			throw new MosipInvalidKeyException(MosipSecurityExceptionCodeConstants.MOSIP_INVALID_KEY_EXCEPTION);
+		} catch (IllegalBlockSizeException e) {
+			throw new MosipInvalidKeyException(MosipSecurityExceptionCodeConstants.MOSIP_INVALID_DATA_EXCEPTION);
+		} catch (BadPaddingException e) {
+			throw new MosipInvalidKeyException(MosipSecurityExceptionCodeConstants.MOSIP_INVALID_DATA_EXCEPTION);
+		}
+	}
+
+	private static byte[] generateIV(int blockSize) {
+		byte[] byteIV = new byte[blockSize];
+		
+		if (Objects.isNull(secureRandom)) 
+			secureRandom = new SecureRandom();
+
+		secureRandom.nextBytes(byteIV);
+		return byteIV;
 	}
 }
