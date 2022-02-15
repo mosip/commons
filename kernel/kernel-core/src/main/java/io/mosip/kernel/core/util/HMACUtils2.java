d13c537ab608073fcc7d39612ca1caf5b5e38eec
urviljoshi <urvvil08@gmail.com>
2022-02-15 19:45:16 +0530

    custom method added got converting byte to hex

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/HMACUtils2.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/util/HMACUtils2.java
@@ -3,27 +3,21 @@
  */
 package io.mosip.kernel.core.util;
 
-import static org.mockito.ArgumentMatchers.nullable;
-
+import java.nio.ByteOrder;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.Base64;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import javax.xml.bind.DatatypeConverter;
-
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
-
-import java.util.Base64;
-
-import java.security.NoSuchAlgorithmException;
 
 /**
- * This class defines the alternate safer HMAC Util to be used in MOSIP Project. The HMAC Util
- * is implemented using desired methods of MessageDigest class of java security
- * package
+ * This class defines the alternate safer HMAC Util to be used in MOSIP Project.
+ * The HMAC Util is implemented using desired methods of MessageDigest class of
+ * java security package
  * 
  * @author Sasikumar Ganesan
  * 
@@ -35,6 +29,11 @@ public final class HMACUtils2 {
 	 */
 	private static final String HASH_ALGORITHM_NAME = "SHA-256";
 	
+	// lookup array for converting byte to hex
+	private static final char[] LOOKUP_TABLE_LOWER = new char[] { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38,
+			0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66 };
+	private static final char[] LOOKUP_TABLE_UPPER = new char[] { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38,
+			0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46 };
 
 	/**
 	 * Performs a digest using the specified array of bytes.
@@ -42,7 +41,7 @@ public final class HMACUtils2 {
 	 * @param bytes bytes to be hash generation
 	 * @return byte[] generated hash bytes
 	 */
-	public static byte[] generateHash(final byte[] bytes) throws NoSuchAlgorithmException{
+	public static byte[] generateHash(final byte[] bytes) throws NoSuchAlgorithmException {
 		MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM_NAME);
 		return messageDigest.digest(bytes);
 	}
@@ -60,7 +59,7 @@ public final class HMACUtils2 {
 		MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM_NAME);
 		messageDigest.update(password);
 		messageDigest.update(salt);
-		return DatatypeConverter.printHexBinary(messageDigest.digest());
+		return encodeByteToHex(messageDigest.digest(), false, ByteOrder.BIG_ENDIAN);
 	}
 
 	/**
@@ -71,7 +70,7 @@ public final class HMACUtils2 {
 	 * @throws NoSuchAlgorithmException
 	 */
 	public static String digestAsPlainText(final byte[] bytes) throws NoSuchAlgorithmException {
-		return DatatypeConverter.printHexBinary(generateHash(bytes)).toUpperCase();
+		return encodeByteToHex(generateHash(bytes), false, ByteOrder.BIG_ENDIAN).toUpperCase();
 	}
 
 	/**
@@ -123,10 +122,10 @@ public final class HMACUtils2 {
 	}
 
 	private static String encode(String password, byte[] salt) {
-		int iterationCount= 27500; //default it has to be higher than this if you want to override
-		if ( System.getenv("hashiteration") != null){
+		int iterationCount = 27500; // default it has to be higher than this if you want to override
+		if (System.getenv("hashiteration") != null) {
 			String envCount = System.getenv("hashiteration");
-			if(Integer.parseInt(envCount) > iterationCount) {
+			if (Integer.parseInt(envCount) > iterationCount) {
 				iterationCount = Integer.parseInt(envCount);
 			}
 		}
@@ -150,4 +149,26 @@ public final class HMACUtils2 {
 			throw new RuntimeException("PBKDF2 algorithm not found", e);
 		}
 	}
+
+	public static String encodeByteToHex(byte[] byteArray, boolean upperCase, ByteOrder byteOrder) {
+
+		// our output size will be exactly 2x byte-array length
+		final char[] buffer = new char[byteArray.length * 2];
+
+		// choose lower or uppercase lookup table
+		final char[] lookup = upperCase ? LOOKUP_TABLE_UPPER : LOOKUP_TABLE_LOWER;
+
+		int index;
+		for (int i = 0; i < byteArray.length; i++) {
+			// for little endian we count from last to first
+			index = (byteOrder == ByteOrder.BIG_ENDIAN) ? i : byteArray.length - i - 1;
+
+			// extract the upper 4 bit and look up char (0-A)
+			buffer[i << 1] = lookup[(byteArray[index] >> 4) & 0xF];
+			// extract the lower 4 bit and look up char (0-A)
+			buffer[(i << 1) + 1] = lookup[(byteArray[index] & 0xF)];
+		}
+		return new String(buffer);
+	}
+
 }
