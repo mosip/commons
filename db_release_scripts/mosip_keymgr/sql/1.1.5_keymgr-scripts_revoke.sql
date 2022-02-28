284e289403170e81880a0d2d42de155f27b36eb3
ckm007 <chandrakeshavmishra@gmail.com>
2022-02-28 07:53:47 +0000

    [DSD-813] added keymanager 1.1.5.5 release scripts

---
+++ db_release_scripts/mosip_keymgr/sql/1.1.5_keymgr-scripts_revoke.sql
@@ -0,0 +1,15 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name	: mosip_keymgr
+-- Release Version 	: 1.1.5
+-- Purpose    		: Revoking Database Alter deployement done for release in Key manager DB.       
+-- Create By   		: Sadanandegowda DM
+-- Created Date		: Feb-2022
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -------------------------------------------------------------------------------------------------
+
+\c mosip_kernel sysadmin
+
+-- TO-DO
+
+-----------------------------------------------------------------------------------------------------
