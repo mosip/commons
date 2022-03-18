7800aca55a80faebdfb536d77c14cb002db96bf5
Urvil Joshi <42868205+urviljoshi@users.noreply.github.com>
2022-03-18 19:44:28 +0530

    request param added for state, cookie set to resp (#1272)
    * request param added for state, cookie set to resp
    
    * added more checks in state
    
    * uuid check added for state

--- kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/controller/LoginController.java
+++ kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/controller/LoginController.java
@@ -1,6 +1,7 @@
 package io.mosip.kernel.authcodeflowproxy.api.controller;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -20,10 +21,12 @@ import io.mosip.kernel.authcodeflowproxy.api.constants.Errors;
 import io.mosip.kernel.authcodeflowproxy.api.dto.AccessTokenResponseDTO;
 import io.mosip.kernel.authcodeflowproxy.api.dto.MosipUserDto;
 import io.mosip.kernel.authcodeflowproxy.api.exception.ClientException;
+import io.mosip.kernel.authcodeflowproxy.api.exception.ServiceException;
 import io.mosip.kernel.authcodeflowproxy.api.service.LoginService;
 import io.mosip.kernel.core.authmanager.model.AuthResponseDto;
 import io.mosip.kernel.core.http.ResponseFilter;
 import io.mosip.kernel.core.http.ResponseWrapper;
+import io.mosip.kernel.core.util.EmptyCheckUtils;
 
 @RestController
 public class LoginController {
@@ -35,15 +38,35 @@ public class LoginController {
 	private LoginService loginService;
 
 	@GetMapping(value = "/login/{redirectURI}")
-	public void login(@CookieValue("state") String state, @PathVariable("redirectURI") String redirectURI,
-			HttpServletResponse res) throws IOException {
-		String uri = loginService.login(redirectURI, state);
+	public void login(@CookieValue(name = "state", required = false) String state,
+			@PathVariable("redirectURI") String redirectURI,
+			@RequestParam(name = "state", required = false) String stateParam, HttpServletResponse res)
+			throws IOException {
+		String stateValue = EmptyCheckUtils.isNullEmpty(state) ? stateParam : state;
+		if (EmptyCheckUtils.isNullEmpty(stateValue)) {
+			throw new ServiceException(Errors.STATE_NULL_EXCEPTION.getErrorCode(),
+					Errors.STATE_NULL_EXCEPTION.getErrorMessage());
+		}
 
+		// there is no UUID.parse method till so using this as alternative
+		try {
+			if (!UUID.fromString(stateValue).toString().equals(stateValue)) {
+				throw new ServiceException(Errors.STATE_NOT_UUID_EXCEPTION.getErrorCode(),
+						Errors.STATE_NOT_UUID_EXCEPTION.getErrorMessage());
+			}
+		} catch (IllegalArgumentException exception) {
+			throw new ServiceException(Errors.STATE_NOT_UUID_EXCEPTION.getErrorCode(),
+					Errors.STATE_NOT_UUID_EXCEPTION.getErrorMessage());
+		}
+		
+		String uri = loginService.login(redirectURI, stateValue);
+		Cookie stateCookie = new Cookie("state", stateValue);
+		stateCookie.setHttpOnly(true);
+		stateCookie.setSecure(true);
+		res.addCookie(stateCookie);
 		res.setStatus(302);
 		res.sendRedirect(uri);
 	}
-
-
 
 	@GetMapping(value = "/login-redirect/{redirectURI}")
 	public void loginRedirect(@PathVariable("redirectURI") String redirectURI, @RequestParam("state") String state,
@@ -55,9 +78,8 @@ public class LoginController {
 		res.addCookie(cookie);
 		res.setStatus(302);
 		String uri = new String(Base64.decodeBase64(redirectURI.getBytes()));
-		res.sendRedirect(uri);	
-		}
-
+		res.sendRedirect(uri);
+	}
 
 	@ResponseFilter
 	@GetMapping(value = "/authorize/admin/validateToken")
@@ -87,7 +109,7 @@ public class LoginController {
 		responseWrapper.setResponse(mosipUserDto);
 		return responseWrapper;
 	}
-	
+
 	@ResponseFilter
 	@DeleteMapping(value = "/logout/user")
 	public ResponseWrapper<AuthResponseDto> logoutUser(
