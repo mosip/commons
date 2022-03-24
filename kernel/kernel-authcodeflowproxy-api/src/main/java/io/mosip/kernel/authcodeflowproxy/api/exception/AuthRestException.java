eb31a9a437c418c15349b88c6b361e4d3dddc852
Urvil Joshi <42868205+urviljoshi@users.noreply.github.com>
2022-03-24 13:12:05 +0530

    Cheery picking from 1.2.0.1 (#1286)
    * request param added for state, cookie set to resp
    
    * conflits resolved
    
    * conflits resolved
    
    * ut commented for short time will resolve with next PR
    
    * added ut and uncommented previous test
    
    * conflits resolved
    
    * making cookiw globally accessible
    
    * status code sent with exception while handling validate token
    
    * conflits resolved

--- kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/exception/AuthRestException.java
+++ kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/exception/AuthRestException.java
@@ -1,8 +1,11 @@
 package io.mosip.kernel.authcodeflowproxy.api.exception;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+
 import io.mosip.kernel.core.exception.BaseUncheckedException;
 import io.mosip.kernel.core.exception.ServiceError;
+import lombok.Getter;
 
 public class AuthRestException extends BaseUncheckedException {
 	/**
@@ -10,6 +13,8 @@ public class AuthRestException extends BaseUncheckedException {
 	 */
 	private static final long serialVersionUID = 8152409863253682472L;
 
+	@Getter
+	private HttpStatus httpStatus;
 	/**
 	 * This variable holds the MosipErrors list.
 	 */
@@ -17,9 +22,11 @@ public class AuthRestException extends BaseUncheckedException {
 
 	/**
 	 * @param list The error list.
+	 * @param httpStatus 
 	 */
-	public AuthRestException(List<ServiceError> list) {
+	public AuthRestException(List<ServiceError> list, HttpStatus httpStatus) {
 		this.list = list;
+		this.httpStatus = httpStatus;
 	}
 
 	/**
