39661c9a054c1c313fcc8d6f185e770055af4030
urviljoshi <urvvil08@gmail.com>
2022-04-13 12:26:03 +0530

    cherry pick conflits resolved

--- kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/constants/Errors.java
+++ kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/constants/Errors.java
@@ -33,9 +33,11 @@ public enum Errors {
 	REST_EXCEPTION("KER-ACP-008", "Exception occured while consuming service"),
 	INVALID_TOKEN("KER-ATH-401", "Authentication Failed : Invalid Token :"),
 	EXCEPTION("KER-ACP-500", "Exception occured "),
-	DOMAIN_EXCEPTION("KER-ACP-009", "uri's domain name not found in allowed domains"),
-	STATE_NULL_EXCEPTION("KER-ACP-009", "state is null or empty"),
-	STATE_NOT_UUID_EXCEPTION("KER-ACP-010", "state is not uuid");
+
+	ALLOWED_URL_EXCEPTION("KER-ACP-009", "uri's not found in allowed url"),
+	STATE_NULL_EXCEPTION("KER-ACP-010", "state is null or empty"),
+	STATE_NOT_UUID_EXCEPTION("KER-ACP-011", "state is not uuid");
+
 
 	/**
 	 * The error code
