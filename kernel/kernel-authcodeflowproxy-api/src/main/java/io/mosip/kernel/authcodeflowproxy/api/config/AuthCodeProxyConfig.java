5963a990413c48f90329caf145a37a4e0970d935
urviljoshi <urvvil08@gmail.com>
2022-09-16 17:09:21 +0530

    MOSIP-23945 bean defination modified for websub and authcode proxy flow

--- kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/config/AuthCodeProxyConfig.java
+++ kernel/kernel-authcodeflowproxy-api/src/main/java/io/mosip/kernel/authcodeflowproxy/api/config/AuthCodeProxyConfig.java
@@ -1,16 +1,31 @@
 package io.mosip.kernel.authcodeflowproxy.api.config;
 
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AuthCodeProxyConfig {
+	
+	@Value("${authcodeproxyflow.httpclient.connections.max.per.host:20}")
+	private int maxConnectionPerRoute;
+
+	@Value("${authcodeproxyflow.httpclient.connections.max:100}")
+	private int totalMaxConnection;
 
 	@ConditionalOnMissingBean(RestTemplate.class)
 	@Bean
 	RestTemplate restTemplate() {
-		return new RestTemplate();
+		HttpClientBuilder httpClientBuilder = HttpClients.custom()
+				.setMaxConnPerRoute(maxConnectionPerRoute)
+				.setMaxConnTotal(totalMaxConnection).disableCookieManagement();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClientBuilder.build());
+		return new RestTemplate(requestFactory);
 	}
 }
