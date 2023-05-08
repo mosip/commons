d01972c7e751d69bbc3b8c1ece5b56a2f55fefa0
ckm007 <chandrakeshavmishra@gmail.com>
2023-05-08 12:04:18 +0530

    [MOSIP-23833] restructered db upgrade scripts

--- db_upgrade_scripts/mosip_kernel/upgrade.sh
+++ db_upgrade_scripts/mosip_kernel/upgrade.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
 set -e
-
-# Parse arguments
-if [[ $# -lt 3 ]]; then
-  echo "Usage: $0 <properties_file> <CURRENT_VERSION> <TARGET_VERSION> <upgrade|revoke>"
-  exit 1
-fi
-
 properties_file="$1"
-CURRENT_VERSION="$2"
-TARGET_VERSION="$3"
-ACTION="$4"
-
-# Read properties file
-if [ -f "$properties_file" ]; then
-  echo "Reading properties from $properties_file"
-  while IFS='=' read -r key value; do
-    key=$(echo $key | tr '.' '_')
-    eval ${key}=\${value}
-  done < "$properties_file"
+echo `date "+%m/%d/%Y %H:%M:%S"` ": $properties_file"
+if [ -f "$properties_file" ]
+then
+     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file \"$properties_file\" found."
+    while IFS='=' read -r key value
+    do
+        key=$(echo $key | tr '.' '_')
+         eval ${key}=\${value}
+    done < "$properties_file"
 else
-  echo "Property file not found, pass property file name as argument."
+     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file not found, Pass property file name as argument."
 fi
 
-echo "Properties file: $properties_file"
 echo "Current version: $CURRENT_VERSION"
-echo "Target version: $TARGET_VERSION"
+echo "UPGRADE version: $UPGRADE_VERSION"
 echo "Action: $ACTION"
 
 # Terminate existing connections
@@ -36,8 +26,8 @@ echo "Terminated connections"
 
 # Execute upgrade or rollback
 if [ "$ACTION" == "upgrade" ]; then
-  echo "Upgrading database from $CURRENT_VERSION to $TARGET_VERSION"
-  UPGRADE_SCRIPT_FILE="sql/${CURRENT_VERSION}_to_${TARGET_VERSION}_upgrade.sql"
+  echo "Upgrading database from $CURRENT_VERSION to $UPGRADE_VERSION"
+  UPGRADE_SCRIPT_FILE="sql/${CURRENT_VERSION}_to_${UPGRADE_VERSION}_upgrade.sql"
   if [ -f "$UPGRADE_SCRIPT_FILE" ]; then
     echo "Executing upgrade script $UPGRADE_SCRIPT_FILE"
     PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f $UPGRADE_SCRIPT_FILE
@@ -46,8 +36,8 @@ if [ "$ACTION" == "upgrade" ]; then
     exit 1
   fi
 elif [ "$ACTION" == "rollback" ]; then
-  echo "Rolling back database for $CURRENT_VERSION to $TARGET_VERSION"
-  REVOKE_SCRIPT_FILE="sql/${CURRENT_VERSION}_to_${TARGET_VERSION}_rollback.sql"
+  echo "Rolling back database for $CURRENT_VERSION to $UPGRADE_VERSION"
+  REVOKE_SCRIPT_FILE="sql/${CURRENT_VERSION}_to_${UPGRADE_VERSION}_rollback.sql"
   if [ -f "$REVOKE_SCRIPT_FILE" ]; then
     echo "Executing rollback script $REVOKE_SCRIPT_FILE"
     PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f $REVOKE_SCRIPT_FILE
