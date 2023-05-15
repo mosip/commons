2d4426028965895d9ed90e9e2b697db10c233b0c
Sowmya Ujjappa Banakar <m1022006@mindtree.com>
2023-05-15 16:16:27 +0530

    MOSIP-27357 updated scripts

--- db_upgrade_scripts/mosip_kernel/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
@@ -1,10 +1,1 @@
 \c mosip_kernel sysadmin
-
-DROP TABLE IF EXISTS kernel.key_alias CASCADE;
-DROP TABLE IF EXISTS kernel.key_policy_def CASCADE;
-DROP TABLE IF EXISTS kernel.key_policy_def_h CASCADE;
-DROP TABLE IF EXISTS kernel.key_store CASCADE;
-DROP TABLE IF EXISTS kernel.sync_control CASCADE;
-DROP TABLE IF EXISTS kernel.sync_job_def CASCADE;
-DROP TABLE IF EXISTS kernel.sync_transaction CASCADE;
-DROP TABLE IF EXISTS kernel.dao_key_store CASCADE;
