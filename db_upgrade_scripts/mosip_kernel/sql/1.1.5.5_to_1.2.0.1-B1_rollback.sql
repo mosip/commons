72b5ee5c2a7f55becf3c92415a1b8ec70081c131
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-06-14 22:27:03 +0530

    [MOSIP-27996]         updated rollback sql's

--- db_upgrade_scripts/mosip_kernel/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
@@ -1,3 +1,5 @@
 \c mosip_kernel
 
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+REASSIGN OWNED BY postgres TO sysadmin;
+
+GRANT SELECT, INSERT, TRUNCATE, REFERENCES, UPDATE, DELETE ON ALL TABLES IN SCHEMA kernel TO sysadmin;
