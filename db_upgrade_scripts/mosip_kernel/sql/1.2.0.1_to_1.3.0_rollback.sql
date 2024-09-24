574909456c0f6506db1b9d498399148f49f136bc
dhanendra06 <dhanendra.tech@gmail.com>
2024-09-24 12:50:26 +0530

    updated the pr based on review comments
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.1_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.1_to_1.3.0_rollback.sql
@@ -1,3 +1,3 @@
 --#INDEX--
-DROP INDEX IF EXISTS uin_status ON kernel.uin;
-DROP INDEX IF EXISTS uin_uin ON kernel.uin;
+DROP INDEX IF EXISTS idx_uin_status ON kernel.uin;
+DROP INDEX IF EXISTS idx_uin_uin ON kernel.uin;
--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.1_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.1_to_1.3.0_upgrade.sql
@@ -1,3 +1,3 @@
 --#INDEX--
-CREATE INDEX IF NOT EXISTS uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
-CREATE INDEX IF NOT EXISTS uin_uin ON kernel.uin using btree(uin);
+CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
+CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
