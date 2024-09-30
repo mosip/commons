5bf79b8db49c3251748191906da89654630eb103
kameshsr <47484458+kameshsr@users.noreply.github.com>
2024-09-30 16:34:16 +0530

    MOSIP-35176 Added loggers (#1573)
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- kernel/kernel-websubclient-api/src/main/java/io/mosip/kernel/websub/api/config/IntentVerificationConfig.java
+++ kernel/kernel-websubclient-api/src/main/java/io/mosip/kernel/websub/api/config/IntentVerificationConfig.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -31,6 +33,8 @@ public class IntentVerificationConfig implements ApplicationContextAware, Embedd
 	private Map<String, String> mappings = null;
 	private StringValueResolver resolver = null;
 
+	private static final Logger logger = LoggerFactory.getLogger(IntentVerificationConfig.class);
+
 	@Override
 	public void setEmbeddedValueResolver(StringValueResolver resolver) {
 		this.resolver = resolver;
@@ -38,6 +42,7 @@ public class IntentVerificationConfig implements ApplicationContextAware, Embedd
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		logger.debug("inside setApplicationContext");
 		mappings = new HashMap<>();
 		for (String beanName : applicationContext.getBeanDefinitionNames()) {
 			//Skip processing this intentVerificationConfig bean.
@@ -47,19 +52,25 @@ public class IntentVerificationConfig implements ApplicationContextAware, Embedd
 			if (!((ConfigurableApplicationContext) applicationContext).getBeanFactory().getBeanDefinition(beanName)
 					.isLazyInit()) {
 				Object obj = applicationContext.getBean(beanName);
+				logger.debug("bean-"+ beanName);
 				Class<?> objClazz = obj.getClass();
+				logger.debug("objClazz"+ objClazz);
 				if (AopUtils.isAopProxy(obj)) {
 
 					objClazz = AopUtils.getTargetClass(obj);
 				}
 
 				for (Method method : objClazz.getDeclaredMethods()) {
+					logger.debug("method name-"+method);
 					if (method.isAnnotationPresent(PreAuthenticateContentAndVerifyIntent.class)) {
 						PreAuthenticateContentAndVerifyIntent preAuthenticateContent = method
 								.getAnnotation(PreAuthenticateContentAndVerifyIntent.class);
 
 						String topic = preAuthenticateContent.topic();
+						logger.debug("topic- "+topic);
+
 						String callback = preAuthenticateContent.callback();
+						logger.debug("callback- "+callback);
 						if (topic.startsWith("${") && topic.endsWith("}")) {
 							topic = resolver.resolveStringValue(topic);
 						}
@@ -68,6 +79,7 @@ public class IntentVerificationConfig implements ApplicationContextAware, Embedd
 							callback = resolver.resolveStringValue(callback);
 						}
 						mappings.put(callback, topic);
+						logger.debug("mapping"+ mappings);
 					}
 				}
 				IntentVerificationFilter intentVerificationFilter = applicationContext
