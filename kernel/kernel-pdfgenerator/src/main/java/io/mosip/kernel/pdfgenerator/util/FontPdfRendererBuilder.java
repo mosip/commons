a86b9d4f2bc2c08fb8b2547a9143d0d90ad90bcd
dhanendra06 <60607841+dhanendra06@users.noreply.github.com>
2025-02-05 11:05:04 +0530

    MOSIP-27719: Added the pdfbox implementation for pdf-generator (#1597)
    * Added the pdfbox implementation for pdf-generator
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    * Fixed the test cases
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    * updated the code based on review comments
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    * modified code based on review comments
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    ---------
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>

---
+++ kernel/kernel-pdfgenerator/src/main/java/io/mosip/kernel/pdfgenerator/util/FontPdfRendererBuilder.java
@@ -0,0 +1,54 @@
+package io.mosip.kernel.pdfgenerator.util;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class FontPdfRendererBuilder {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FontPdfRendererBuilder.class);
+
+    private static PdfRendererBuilder builderInstance;
+
+    private FontPdfRendererBuilder() {
+        // Private constructor to enforce singleton
+    }
+
+    public static synchronized PdfRendererBuilder getBuilder(String ttfFilePath) throws IOException {
+        if (builderInstance == null) {
+            builderInstance = new PdfRendererBuilder();
+            initializeFonts(builderInstance,ttfFilePath);
+        }
+        return builderInstance;
+    }
+
+    private static void initializeFonts(PdfRendererBuilder builder,String ttfFilePath) throws IOException {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(ttfFilePath);
+
+        if (resources.length == 0) {
+            LOGGER.info("Font family not found taking default font");
+        }else {
+
+            File tempFontDir = Files.createTempDirectory("loaded-fonts").toFile();
+            tempFontDir.deleteOnExit();
+
+            for (Resource resource : resources) {
+                try (InputStream fontStream = resource.getInputStream()) {
+                    File tempFontFile = new File(tempFontDir, resource.getFilename());
+                    Files.copy(fontStream, tempFontFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+                    LOGGER.info("Loading font: {}", tempFontFile.getAbsolutePath());
+                    builder.useFont(tempFontFile, resource.getFilename().replace(".ttf", ""));
+                }
+            }
+        }
+    }
+}
