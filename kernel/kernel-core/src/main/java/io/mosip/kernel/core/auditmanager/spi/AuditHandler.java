8f61680abce2bf568d306f5150412ed50e4afa10
kameshsr <47484458+kameshsr@users.noreply.github.com>
2025-08-29 16:54:03 +0530

    Mosip-42357 Added method (#1701)
    * MOSIP-42357 Added log
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42357 Added log
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42698 Corrected cryptoutil
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42357 added log
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42698 Added log
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42698 Added log
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42334 corrected dateutils
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42334 corrected dateutil
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42357 corrected date utils
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42357 corrected Dateutils method
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * MOSIP-42357 Added method to AuditHandler
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    ---------
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- kernel/kernel-core/src/main/java/io/mosip/kernel/core/auditmanager/spi/AuditHandler.java
+++ kernel/kernel-core/src/main/java/io/mosip/kernel/core/auditmanager/spi/AuditHandler.java
@@ -1,8 +1,11 @@
 package io.mosip.kernel.core.auditmanager.spi;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * Interface with function to write AuditRequest
- * 
+ *
  * @author Dharmesh Khandelwal
  * @since 1.0.0
  *
@@ -11,10 +14,33 @@ public interface AuditHandler<T> {
 
 	/**
 	 * Function to write AuditRequest
-	 * 
+	 *
 	 * @param auditRequest The AuditRequest
 	 * @return true - if AuditRequest is successfully written
 	 */
 	boolean addAudit(T auditRequest);
 
+	/**
+	 * Function to write multiple AuditRequests in a batch
+	 *
+	 * @param auditRequests The list of AuditRequests
+	 * @return true - if AuditRequests are successfully written
+	 */
+	boolean addAudits(List<T> auditRequests);
+
+	/**
+	 * Function to update multiple AuditRequests in a batch
+	 *
+	 * @param auditRequests The list of AuditRequests to update
+	 * @return true - if AuditRequests are successfully updated
+	 */
+	boolean updateAudits(List<T> auditRequests);
+
+	/**
+	 * Function to delete audits older than the specified cutoff time
+	 *
+	 * @param cutoffTime The cutoff time for deleting old audits
+	 * @return true - if old audits are successfully deleted
+	 */
+	boolean deleteAuditsOlderThan(LocalDateTime cutoffTime);
 }
