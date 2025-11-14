94e72b97b3ed7476a7b2efb683b8c7234bfa3226
Gokulraj C <110164849+GOKULRAJ136@users.noreply.github.com>
2025-11-14 14:39:08 +0530

    MOSIP-42305 - Added indexes for performance optimization (#1720)
    * MOSIP-42305 - Added indexes for performance optimization
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
    
    * Removed non relevant indexes
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
    
    * Added Rollback
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- db_scripts/mosip_kernel/ddl/kernel-vid.sql
+++ db_scripts/mosip_kernel/ddl/kernel-vid.sql
@@ -35,3 +35,7 @@ COMMENT ON COLUMN kernel.vid.is_deleted IS 'IS_Deleted : Flag to mark whether th
 -- ddl-end --
 COMMENT ON COLUMN kernel.vid.del_dtimes IS 'Deleted DateTimestamp : Date and Timestamp when the record is soft deleted with is_deleted=TRUE';
 -- ddl-end --
+
+-- PERFORMANCE OPTIMIZATION INDEXES
+CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
+CREATE INDEX CONCURRENTLY idx_vid_status_isdeleted ON kernel.vid (vid_status, is_deleted);
--- db_upgrade_scripts/mosip_kernel/sql/1.3.0-B2_to_1.3.0-B3_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.3.0-B2_to_1.3.0-B3_upgrade.sql
@@ -1,1 +1,6 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+-- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
+
+CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
+CREATE INDEX CONCURRENTLY idx_vid_status_isdeleted ON kernel.vid (vid_status, is_deleted);
+
+---END UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES--
