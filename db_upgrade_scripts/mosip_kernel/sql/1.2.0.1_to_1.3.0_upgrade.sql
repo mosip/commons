a6769706d4331d92988bdd6ebf5d41f9ce61940f
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2025-11-14 15:19:46 +0530

    Updated Upgrade and Rollback scripts for index [MOSIP-42305]
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.1_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.1_to_1.3.0_upgrade.sql
@@ -1,3 +1,10 @@
 --#INDEX--
 CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
+
+-- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
+
+CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
+CREATE INDEX CONCURRENTLY idx_vid_status_isdeleted ON kernel.vid (vid_status, is_deleted);
+
+---END UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES--
