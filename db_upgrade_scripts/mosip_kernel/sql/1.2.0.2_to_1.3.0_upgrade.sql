68d800b0da5376d07bdad8b1223e480b2c2ef4f6
kameshsr <kameshsr1338@gmail.com>
2025-11-20 19:55:05 +0530

    MOSIP-43688 added db name in upgrade script
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -1,3 +1,5 @@
+\c mosip_kernel
+
 --#INDEX--
 CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
