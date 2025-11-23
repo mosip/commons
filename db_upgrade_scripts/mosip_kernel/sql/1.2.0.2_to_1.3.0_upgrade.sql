f2aa120e79e15a31ff9fbeee57cef124c95b8a87
Nidhi <nidhi.k@cyberpwn.com>
2025-11-23 14:55:35 +0530

    [MOSIP-43787] Added 1.3.0-beta.3 to 1.3.0 upgrade and rollback for kernel.vid
    Added 1.3.0-beta.3 to 1.3.0 upgrade and rollback for kernel.vid.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -4,7 +4,7 @@
 CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
 
--- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
+-- Below script required to rollback from 1.3.0-beta.3 to 1.3.0.
 
 CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
 CREATE INDEX CONCURRENTLY idx_vid_status_isdeleted ON kernel.vid (vid_status, is_deleted);
