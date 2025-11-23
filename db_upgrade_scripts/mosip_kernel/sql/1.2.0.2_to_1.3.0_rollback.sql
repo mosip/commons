ec3cfaf95dcebbf2104368676cfc70fda392e4fd
Nidhi <nidhi.k@cyberpwn.com>
2025-11-23 14:57:35 +0530

    Update rollback script for version 1.3.0
    Added rollback script for performance optimization indexes.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_rollback.sql
@@ -4,7 +4,7 @@
 DROP INDEX IF EXISTS idx_uin_status ON kernel.uin;
 DROP INDEX IF EXISTS idx_uin_uin ON kernel.uin;
 
--- ROLLBACK FOR PERFORMANCE OPTIMIZATION INDEXES
+-- Below script required to rollback from 1.3.0-beta.3 to 1.3.0.
 
 DROP INDEX IF EXISTS kernel.idx_vid_status_not_deleted;
 
