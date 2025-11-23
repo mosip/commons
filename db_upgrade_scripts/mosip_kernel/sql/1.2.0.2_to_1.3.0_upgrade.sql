17ea2d8b38982750627e4393476cf4e0ecb2b224
Nidhi <nidhi.k@cyberpwn.com>
2025-11-23 21:39:03 +0530

    Add performance upgrade
    Added performance optimization indexes for vid_status.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -5,8 +5,9 @@ CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) 
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
 
 -- Below script required to rollback from 1.3.0-beta.3 to 1.3.0.
+-- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
 
 CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
 CREATE INDEX CONCURRENTLY idx_vid_status_isdeleted ON kernel.vid (vid_status, is_deleted);
 
-
+---END UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES--
