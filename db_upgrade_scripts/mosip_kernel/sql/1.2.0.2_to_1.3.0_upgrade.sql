644883a5272b0796187df8f00d60d8e54f5859b3
Nidhi <nidhi.k@cyberpwn.com>
2025-11-23 15:02:25 +0530

    Remove comment for performance optimization indexes upgrade script
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -9,4 +9,4 @@ CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
 CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
 CREATE INDEX CONCURRENTLY idx_vid_status_isdeleted ON kernel.vid (vid_status, is_deleted);
 
----END UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES--
+
