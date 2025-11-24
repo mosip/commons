f8599847f8bb3fb7ac6f2e59ebd4f80dd04c7350
Nidhi <nidhi.k@cyberpwn.com>
2025-11-24 15:00:14 +0530

    changed to upgrade
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -4,7 +4,7 @@
 CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
 
--- Below script required to rollback from 1.3.0-beta.3 to 1.3.0.
+-- Below script required to upgrade from 1.3.0-beta.3 to 1.3.0.
 -- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
 
 CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
