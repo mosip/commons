2c9d8699846b2cb20f4c258908f076a55dfbb908
Nidhi <nidhi.k@cyberpwn.com>
2025-11-24 16:19:48 +0530

    [MOSIP-43787] Added 1.3.0-beta.3 to 1.3.0 upgrade and rollback (#1752)
    * [MOSIP-43787] Added 1.3.0-beta.3 to 1.3.0 upgrade and rollback for kernel.vid
    
    Added 1.3.0-beta.3 to 1.3.0 upgrade and rollback for kernel.vid.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    * Update rollback script for version 1.3.0
    
    Added rollback script for performance optimization indexes.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    * Remove rollback for performance optimization indexes
    
    Remove rollback commands for performance optimization indexes.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    * Remove comment for performance optimization indexes upgrade script
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    * Add performance upgrade
    
    Added performance optimization indexes for vid_status.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    * performance rollback  comments
    
    Added comments to clarify rollback for performance optimization indexes.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    * changed to upgrade
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>
    
    ---------
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_rollback.sql
@@ -4,6 +4,7 @@
 DROP INDEX IF EXISTS idx_uin_status ON kernel.uin;
 DROP INDEX IF EXISTS idx_uin_uin ON kernel.uin;
 
+-- Below script required to rollback from 1.3.0-beta.3 to 1.3.0.
 -- ROLLBACK FOR PERFORMANCE OPTIMIZATION INDEXES
 
 DROP INDEX IF EXISTS kernel.idx_vid_status_not_deleted;
--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -4,6 +4,7 @@
 CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
 
+-- Below script required to upgrade from 1.3.0-beta.3 to 1.3.0.
 -- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
 
 CREATE INDEX idx_vid_status_not_deleted ON kernel.vid (vid_status) WHERE is_deleted = false;
