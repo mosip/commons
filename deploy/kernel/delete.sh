685ae022815c210115a3a1488f5f7801c184a56c
Swapnil <swapnil.mohanty@technoforte.co.in>
2025-11-28 17:44:51 +0530

    [MOSIP-43861] Add helm delete commands for additional kernel charts
    Signed-off-by: Swapnil <swapnil.mohanty@technoforte.co.in>

--- deploy/kernel/delete.sh
+++ deploy/kernel/delete.sh
@@ -12,6 +12,11 @@ function deleting_kernel() {
       read -p "Are you sure you want to delete all kernel helm charts?(Y/n) " yn
       if [ $yn = "Y" ]
         then
+          helm -n $NS delete authmanager
+          helm -n $NS delete auditmanager
+          helm -n $NS delete masterdata
+          helm -n $NS delete otpmanager
+          helm -n $NS delete syncdata
           helm -n $NS delete idgenerator
           helm -n $NS delete pridgenerator
           helm -n $NS delete ridgenerator
