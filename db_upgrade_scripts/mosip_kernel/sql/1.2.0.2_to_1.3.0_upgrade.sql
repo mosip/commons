6661954118e7eba163c8b52938dc4ef09293f556
Nandhukumar <nandhukumare@gmail.com>
2025-12-05 15:35:18 +0530

    MOSIP-43917 | updated missed index script
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_kernel/sql/1.2.0.2_to_1.3.0_upgrade.sql
@@ -3,6 +3,12 @@
 --#INDEX--
 CREATE INDEX IF NOT EXISTS idx_uin_status ON kernel.uin using btree(uin_status) where uin_status='UNUSED';
 CREATE INDEX IF NOT EXISTS idx_uin_uin ON kernel.uin using btree(uin);
+CREATE INDEX IF NOT EXISTS idx_uin_status_all ON kernel.uin USING btree (uin_status);
+CREATE INDEX IF NOT EXISTS idx_uin_status_crdtime ON kernel.uin USING btree (uin_status, cr_dtimes);
+CREATE INDEX IF NOT EXISTS idx_uin_status_isdeleted ON kernel.uin USING btree (uin_status, is_deleted);
+CREATE INDEX IF NOT EXISTS idx_vid_status_available_not_deleted ON kernel.vid USING btree (vid_status) WHERE vid_status = 'AVAILABLE' AND is_deleted = false;
+CREATE INDEX IF NOT EXISTS idx_vid_status_crdtime ON kernel.vid USING btree (vid_status, cr_dtimes);
+CREATE INDEX IF NOT EXISTS idx_vid_status_unused ON kernel.vid USING btree (cr_dtimes) WHERE vid_status = 'UNUSED';
 
 -- Below script required to upgrade from 1.3.0-beta.3 to 1.3.0.
 -- UPGRADE FOR PERFORMANCE OPTIMIZATION INDEXES
